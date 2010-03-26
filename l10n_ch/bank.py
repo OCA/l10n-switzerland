0cff908fb5d0f3961dba170b1f835929f69025c1
Joël Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2010-03-26 14:40:18 +0100

    [ADD] First commit to initiate the project. Those modules are taken from addons and extra-addons official OpenERP branch. We'll continue to develop it here, and merge it when required into the official OpenERP's branches

---
+++ l10n_ch/bank.py
@@ -0,0 +1,55 @@
+#
+#  bank.py
+#  l10n_ch
+#
+#  Created by Nicolas Bessi based on Credric Krier contribution
+#
+#  Copyright (c) 2009 CamptoCamp. All rights reserved.
+##############################################################################
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+from osv import fields, osv
+
+
+class Bank(osv.osv):
+    """Inherit res.bank class in order to add swiss specific field"""
+    _inherit = 'res.bank'
+    _columns = {
+        ###Swiss unik bank identifier also use in IBAN number
+        'clearing': fields.char('Clearing number', size=64),
+        ### city of the bank
+        'city': fields.char('City', size=128, select=1),
+    }
+
+Bank()
+
+class bvr_checkbox(osv.osv):
+    """ Add function to generate function """
+    _inherit = "res.partner.bank"
+    _columns = {
+        'printbank' : fields.boolean('Print Bank on BVR'),
+        'printaccount' : fields.boolean('Print Account Number on BVR'),
+        }
+bvr_checkbox()
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
