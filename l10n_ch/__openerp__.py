34baf8ebd6f220be0fafd9da41c87b7d4f405198
unknown <nicolas.bessi@camptocamp.com>
2011-06-10 09:35:09 +0200

    [IMP] of terp + PEP

--- l10n_ch/__openerp__.py
+++ l10n_ch/__openerp__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Author: Nicolas Bessi. Copyright Camptocamp SA
-#    Donors: Hasa Sàrl, Open Net Sàrl and Prisme Solutions Informatique SA
+#    Donors: Hasa SA, Open Net SA and Prisme Solutions Informatique SA
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -22,7 +22,9 @@
 {
     "name" : "Switzerland - localization with 2011 taxes",
     "description" : """
-Swiss localisation :
+Swiss localisation.
+===================
+
  - DTA generation for a lot of payment types
  - BVR management (number generation, report, etc..)
  - Import account move from the bank file (like v11 etc..)
@@ -31,27 +33,24 @@ Swiss localisation :
 You can also add ZIP and bank completion with:
  - l10n_ch_zip
  - l10n_ch_bank
- 
- Author: Camptocamp SA
- Donors: Hasa Sàrl, Open Net Sàrl and Prisme Solutions Informatique SA
 
-------------------------------------------------------------------------
+Author: Camptocamp SA
+Donors: Hasa SA, Open Net SA and Prisme Solutions Informatique SA
 
-Module incluant la localisation Suisse de TinyERP revu et corrigé par Camptocamp. Cette nouvelle version
-comprend la gestion et l'émissionde BVR, le paiement électronique via DTA (pour les banques, le système postal est en développement)
-et l'import du relevé de compte depuis la banque de manière automatisée.
-De plus, nous avons intégré la définition de toutes les banques Suisses(adresse, swift et clearing).
+Module incluant la localisation Suisse de TinyERP revu et corrig√© par Camptocamp. Cette nouvelle version
+comprend la gestion et l'√©missionde BVR, le paiement √©lectronique via DTA (pour les banques, le syst√®me postal est en d√©veloppement)
+et l'import du relev√© de compte depuis la banque de mani√®re automatis√©e.
+De plus, nous avons int√©gr√© la d√©finition de toutes les banques Suisses(adresse, swift et clearing).
 
-Par ailleurs, conjointement à ce module, nous proposons la complétion NPA:
+Par ailleurs, conjointement √† ce module, nous proposons la compl√©tion NPA:
 
 Vous pouvez ajouter la completion des banques et des NPA avec with:
  - l10n_ch_zip
  - l10n_ch_bank
- 
- Auteur: Camptocamp SA
- Donateurs: Hasa Sàrl, Open Net Sàrl and Prisme Solutions Informatique SA
 
---------------------------------------------------------------------------
+ Auteur: Camptocamp SA
+ Donateurs: Hasa S√†rl, Open Net S√†rl and Prisme Solutions Informatique SA
+
 TODO :
 - Implement bvr import partial reconciliation
 - Replace wizard by osv_memory when possible
@@ -63,7 +62,7 @@ TODO :
 """,
     "version" : "6.0",
     "author" : "Camptocamp SA",
-    "category" : "Localization/Account Charts",
+    "category" : "Finance",
     "website": "http://www.camptocamp.com",
 
     "depends" : [
@@ -102,5 +101,6 @@ TODO :
     "active": False,
     "installable": True,
     "certificate" : "001103836064567088989",
+    'images': ['images/config_chart_l10n_ch.jpeg','images/l10n_ch_chart.jpeg'],
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
