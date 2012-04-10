6a0a2645bda60c901f41f5242e9181e8a88ac4e9
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2012-04-10 15:19:34 +0200

    [FIX] l10n_ch - postal number check

--- l10n_ch/bank.py
+++ l10n_ch/bank.py
@@ -89,8 +89,11 @@ class ResPartnerBank(osv.osv):
         pattern = r'^[0-9]{2}-[0-9]{1,6}-[0-9]$'
         if not re.search(pattern, number):
             return False
-        num, checksum = (number.replace('-','')[:-1], number[-1:])
-        return mod10r(num)[-1:] == checksum
+        nums = number.split('-')
+        prefix = nums[0]
+        num = nums[1].rjust(6,'0')
+        checksum = nums[2]
+        return mod10r(prefix + num)[-1:] == checksum
 
 
     def _check_5_pos_postal_num(self, number):
