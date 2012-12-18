e039fba839169486f5ec01ccf17740f05e1dcd08
unknown <nicolas.bessi@camptocamp.com>
2012-12-18 12:42:54 +0100

    [IMP] cleanup of taxes

--- l10n_ch/__openerp__.py
+++ l10n_ch/__openerp__.py
@@ -42,7 +42,7 @@ Due to important refactoring needs and the Switzerland adoption of new internati
 - **l10n_ch_payment_slip**: Support of ESR/BVR payment slip report and reconciliation. Report refactored with easy element positioning.
 - **l10n_ch_sepa**: Alpha implementation of PostFinance SEPA/PAIN support will be completed during 2013/2014
 
-The modules will be soon available on OpenERP swiss localisation on launchpad:
+The modules will be soon available on OpenERP swiss localization on launchpad:
 https://launchpad.net/openerp-swiss-localization
 """,
  'version': '7.0',
