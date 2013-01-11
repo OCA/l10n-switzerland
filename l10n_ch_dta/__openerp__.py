9a6082e6c82df8537c500d62fa6c010e6873c38e
unknown <nicolas.bessi@camptocamp.com>
2013-01-11 11:12:06 +0100

    [ADD] missing files

---
+++ l10n_ch_dta/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi. Copyright Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Switzerland - Bank Payment File (DTA)',
+ 'description':  """
+Swiss localization supplier bank payment file known as DTA file :
+=================================================
+Generate a DTA file for swiss banking system from payment order
+
+""",
+ 'version': '1.0',
+ 'author': 'Camptocamp',
+ 'category': 'Localization',
+ 'website': 'http://www.camptocamp.com',
+ 'depends': ['base', 'account_payment', 'l10n_ch_base_bank'],
+ 'data': ["wizard/create_dta_view.xml"],
+ 'demo': ["demo/dta_demo.xml"],
+ 'test': [], # To be ported or migrate to unit tests or scenarios
+ 'auto_install': False,
+ 'installable': True,
+ 'images': []
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
