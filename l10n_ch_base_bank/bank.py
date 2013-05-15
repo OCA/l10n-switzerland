460907ce194961e43c23d66af7970ea484fe29d5
unknown <nicolas.bessi@camptocamp.com>
2013-05-15 15:04:57 +0200

    [IMP] add notion of CCP on bank instead of having an account on res.partner.bank. The code is retro compatible and if you do not have a ccp on bank it will act as before

--- l10n_ch_base_bank/bank.py
+++ l10n_ch_base_bank/bank.py
@@ -20,39 +20,11 @@
 #
 ##############################################################################
 import re
-from openerp.osv.orm import Model, fields
+from openerp.osv import orm, fields
 from tools import mod10r
 
 
-class Bank(Model):
-    """Inherit res.bank class in order to add swiss specific field"""
-    _inherit = 'res.bank'
-    _columns = {
-        ### Internal reference
-        'code': fields.char('Code', size=64),
-        ###Swiss unik bank identifier also use in IBAN number
-        'clearing': fields.char('Clearing number', size=64),
-        ### city of the bank
-        'city': fields.char('City', size=128, select=1),
-    }
-
-
-class ResPartnerBank(Model):
-    """
-    Inherit res.partner.bank class in order to add swiss specific fields and state controls
-    """
-    _inherit = "res.partner.bank"
-
-    _columns = {
-        'name': fields.char('Description', size=128, required=True),
-        'bvr_adherent_num': fields.char('Bank BVR adherent number', size=11,
-                                        help=("Your Bank adherent number to be printed in references of your BVR."
-                                              "This is not a postal account number.")),
-        'acc_number': fields.char('Account/IBAN Number', size=64, required=True),
-        'post_related_account': fields.char('Post related account',
-                                            size=64, required=True,
-                                            help="Bank account related to bank ccp")
-    }
+class BankCommon(object):
 
     def _check_9_pos_postal_num(self, number):
         """
@@ -79,20 +51,120 @@ class ResPartnerBank(Model):
             return False
         return True
 
+class Bank(orm.Model, BankCommon):
+    """Inherit res.bank class in order to add swiss specific field"""
+    _inherit = 'res.bank'
+    _columns = {
+        ### Internal reference
+        'code': fields.char('Code', size=64),
+        ###Swiss unik bank identifier also use in IBAN number
+        'clearing': fields.char('Clearing number', size=64),
+        ### city of the bank
+        'city': fields.char('City', size=128, select=1),
+        ### ccp of the bank
+        'ccp': fields.char('CCP', size=64, select=1)
+    }
+
+    def _check_ccp_duplication(self, cursor, uid, ids):
+        p_acc_obj = self.pool['res.partner.bank']
+        for bank in self.browse(cursor, uid, ids):
+            p_acc_ids = p_acc_obj.search(cursor, uid, [('bank', '=', bank.id)])
+            if p_acc_ids:
+                check = p_acc_obj._check_ccp_duplication(cursor, uid, p_acc_ids)
+                if not check:
+                    return False
+        return True
+
+
     def _check_postal_num(self, cursor, uid, ids):
         """
         validate postal number format
         """
         banks = self.browse(cursor, uid, ids)
-        for b in banks:
-            if not b.state in ('bv', 'bvr'):
-                return True
-            return self._check_9_pos_postal_num(b.acc_number) or \
-                   self._check_5_pos_postal_num(b.acc_number)
+        for bank in banks:
+            if not bank.ccp:
+                continue
+            if not (self._check_9_pos_postal_num(bank.ccp) or
+                    self._check_5_pos_postal_num(bank.ccp)):
+                return False
+        return True
 
     _constraints = [(_check_postal_num,
                     'Please enter a correct postal number. (01-23456-1 or 12345)',
-                    ['acc_number'])]
+                    ['ccp']),
+
+                    (_check_ccp_duplication,
+                    'You can not enter a ccp both on the bank and on an account'
+                    ' of type BV, BVR',
+                    ['acc_number', 'bank'])]
+
+class ResPartnerBank(orm.Model, BankCommon):
+    """
+    Inherit res.partner.bank class in order to add swiss specific fields and state controls
+    """
+    _inherit = 'res.partner.bank'
+
+    _columns = {
+        'name': fields.char('Description', size=128, required=True),
+        'bvr_adherent_num': fields.char('Bank BVR adherent number', size=11,
+                                        help=("Your Bank adherent number to be printed in references of your BVR."
+                                              "This is not a postal account number.")),
+        'acc_number': fields.char('Account/IBAN Number', size=64, required=True),
+        'ccp': fields.related('bank', 'ccp', type='char', string='CCP'),
+    }
+
+    def get_account_number(self, cursor, uid, bid, context=None):
+        if isinstance(bid, list):
+            bid = bid[0]
+        current = self.browse(cursor, uid, bid, context=context)
+        if current.state not in ('bv', 'bvr'):
+            return current.acc_number
+        if current.bank and current.bank.ccp:
+            return current.bank.ccp
+        else:
+            return current.acc_number
+
+    def _check_postal_num(self, cursor, uid, ids):
+        """
+        validate postal number format
+        """
+        p_banks = self.browse(cursor, uid, ids)
+        for p_bank in p_banks:
+            if not p_bank.state in ('bv', 'bvr'):
+                continue
+            if not (self._check_9_pos_postal_num(p_bank.get_account_number()) or
+                    self._check_5_pos_postal_num(p_bank.get_account_number())):
+                return False
+        return True
+
+    def _check_ccp_duplication(self, cursor, uid, ids):
+        """
+          Ensure that there is not a ccp in bank and res partner bank
+          at same time
+        """
+        p_banks = self.browse(cursor, uid, ids)
+        for p_bank in p_banks:
+            if not p_bank.state in ('bv', 'bvr'):
+                continue
+            bank_ccp = p_bank.bank.ccp if p_bank.bank else False
+            if not bank_ccp:
+                continue
+            part_bank_check = (self._check_5_pos_postal_num(p_bank.acc_number) or
+                               self._check_9_pos_postal_num(p_bank.acc_number))
+            bank_check = (self._check_5_pos_postal_num(p_bank.bank.ccp) or
+                          self._check_9_pos_postal_num(p_bank.bank.ccp))
+            if part_bank_check and bank_check:
+                return False
+        return True
+
+    _constraints = [(_check_postal_num,
+                    'Please enter a correct postal number. (01-23456-1 or 12345)',
+                    ['acc_number']),
+
+                    (_check_ccp_duplication,
+                    'You can not enter a ccp both on the bank and on an account'
+                    ' of type BV, BVR',
+                    ['acc_number', 'bank'])]
 
     _sql_constraints = [('bvr_adherent_uniq', 'unique (bvr_adherent_num)',
                          'The BVR adherent number must be unique !')]
