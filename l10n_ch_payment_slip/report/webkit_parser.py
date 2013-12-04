cb10f666d04f8f18a6152b59846cee879131d326
Vincent Renaville <vincent.renaville@camptocamp.com>
2013-12-04 10:31:52 +0100

    [IMP] Slipt create_single_pdf with a new function get_obj_reference to be able to overload this function, in order to change the source of data

--- l10n_ch_payment_slip/report/webkit_parser.py
+++ l10n_ch_payment_slip/report/webkit_parser.py
@@ -26,12 +26,18 @@ class MultiBvrWebKitParser(webkit_report.WebKitParser):
     def create_single_pdf(self, cursor, uid, ids, data, report_xml, context=None):
         self.pool = pooler.get_pool(cursor.dbname)
         target_obj = 'account.move.line'
-        move_line_obj = self.pool.get(target_obj)
+        move_lines = self.get_obj_reference(cursor, uid, ids,context)
+        context['active_model'] = self.table = target_obj
+        context['active_ids'] = ids = move_lines
+        import pdb
+        pdb.set_trace()
+        return super(MultiBvrWebKitParser, self).create_single_pdf(cursor, uid, ids, data, report_xml, context)
+    
+    def get_obj_reference(self,cursor,uid,ids,context):
+        move_line_obj = self.pool.get('account.move.line')
         account_obj = self.pool.get('account.account')
         invoice_obj = self.pool.get('account.invoice')
         inv = invoice_obj.browse(cursor, uid, ids[0],context)
         tier_account_id = account_obj.search(cursor, uid, [('type', 'in', ['receivable', 'payable'])])
         move_lines = move_line_obj.search(cursor, uid, [('move_id', '=', inv.move_id.id), ('account_id', 'in', tier_account_id)])
-        context['active_model'] = self.table = target_obj
-        context['active_ids'] = ids = move_lines
-        return super(MultiBvrWebKitParser, self).create_single_pdf(cursor, uid, ids, data, report_xml, context)
+        return move_lines
