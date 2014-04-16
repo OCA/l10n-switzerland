539760b4796326b0f668ed884ece21e5c769a00c
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-04-16 16:25:35 +0200

    [REFATOR] credit control payment slip to support crdit.control.line and move line relation cardinality

--- l10n_ch_credit_control_payment_slip_report/account.py
+++ l10n_ch_credit_control_payment_slip_report/account.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author Vincent Renaville. Copyright 2013 Camptocamp SA
+#    Author: Nicolas Bessi
+#    Copyright 2014 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -17,6 +18,23 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import credit_control_communication
-from . import credit_control_printer
-from . import account
+from openerp.osv import orm
+
+
+class account_move_line(orm.Model):
+    """Overrride BVR amount to take in account dunning fees"""
+
+    _inherit = "account.move.line"
+
+    def _get_bvr_amount(self, cr, uid, move, rtype=None):
+        """Hook to get amount in CHF for BVR
+        The amount must include dunning fees
+
+        :param move: move line report
+        :param rtype: report type string just in case
+
+        :returns: BVR float amount
+
+        """
+        fees = getattr(move, 'bvr_dunning_fees', 0.0)
+        return move.debit + fees
--- l10n_ch_credit_control_payment_slip_report/credit_control_printer.py
+++ l10n_ch_credit_control_payment_slip_report/credit_control_printer.py
@@ -28,6 +28,7 @@ class CreditControlPrinter(orm.TransientModel):
     _inherit = 'credit.control.printer'
 
     def print_linked_bvr(self, cr, uid, wiz_id, context=None):
+        """Print BVR from credit line"""
         assert not (isinstance(wiz_id, list) and len(wiz_id) > 1), \
             "wiz_id: only one id expected"
         comm_obj = self.pool.get('credit.control.communication')
@@ -38,13 +39,9 @@ class CreditControlPrinter(orm.TransientModel):
         if not form.line_ids and not form.print_all:
             raise orm.except_orm(_('Error'),
                                  _('No credit control lines selected.'))
-
-        move_line_ids = []
-        for line in form.line_ids:
-            if line.move_line_id:
-                move_line_ids.append(line.move_line_id.id)
+        credit_ids = [x.id for x in form.line_ids]
         report_file = comm_obj._generate_report_bvr(cr, uid,
-                                                    move_line_ids,
+                                                    credit_ids,
                                                     context=context)
 
         form.write({'report_file': base64.b64encode(report_file),
