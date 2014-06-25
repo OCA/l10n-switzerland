1d63cb65cb2d2975a760a4319d1eed515018ad0b
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-06-25 16:24:19 +0200

    fix indent

--- l10n_ch_payment_slip/partner.py
+++ l10n_ch_payment_slip/partner.py
@@ -26,9 +26,7 @@ class ResPartner(orm.Model):
     _inherit = 'res.partner'
 
     _columns = {
-        'ref_companies': fields.one2many('res.company', 'partner_id',
-        'Companies that refers to partner'),
+        'ref_companies': fields.one2many(
+            'res.company', 'partner_id',
+            'Companies that refers to partner'),
     }
-
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
