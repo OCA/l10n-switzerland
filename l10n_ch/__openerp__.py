c00f5069d58d79efe9ffa47a5fa61f568aa1361b
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-08-12 09:05:11 +0200

    [FIX] PEP8 and convention for l10n_ch

--- l10n_ch/__openerp__.py
+++ l10n_ch/__openerp__.py
@@ -24,7 +24,7 @@
 
 {'name': 'Switzerland - Accounting',
  'summary': 'Multilang swiss STERCHI account chart and taxes',
- 'description':  """
+ 'description': """
 Swiss localization
 ==================
 
