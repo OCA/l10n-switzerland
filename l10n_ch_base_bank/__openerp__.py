15ba7ba319edb87638934a2d68c148f6b39615e2
Paul Catinean <paulcatinean@gmail.com>
2014-10-15 09:40:50 +0300

    [ADD] Added l10n_ch_base_bank migrated to the new api

---
+++ l10n_ch_base_bank/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi. Copyright Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Switzerland - Bank type',
+ 'summary': 'Types and number validation for swiss electronic pmnt. DTA, ESR',
+ 'description': """
+Swiss bank type and fields
+==========================
+
+This addons will add different bank types required by specific swiss electronic
+payment like DTA and ESR. It allows to manage both Post and Bank systems.
+
+It'll perform some validation when entring bank account number or ESR number
+in invoice and add some Swiss specific fields on bank.
+
+This module is required if you want to use electornic payment in Switzerland.
+""",
+ 'version': '1.1',
+ 'author': 'Camptocamp',
+ 'category': 'Localization',
+ 'website': 'http://www.camptocamp.com',
+ 'depends': ['account'],
+ 'data': ['bank_view.xml', 'bank_data.xml'],
+ 'demo': [],
+ 'test': [],
+ 'auto_install': False,
+ 'installable': True,
+ 'images': []
+ }
