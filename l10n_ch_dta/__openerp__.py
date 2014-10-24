968bf5cdaf3acb4115cede1d87dcbe22beddf235
hurrinico <nicola.malcontenti@agliebg.com>
2014-10-24 16:37:23 +0200

    [ADD] l10n_ch_dta ported to the new api

---
+++ l10n_ch_dta/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi. Copyright Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Switzerland - Bank Payment File (DTA)',
+ 'summary': 'Electronic payment file for Swiss bank (DTA)',
+ 'description': """
+Swiss bank electronic payment (DTA)
+===================================
+
+This addons allows you to generate an electronic payment file for Swiss bank
+(known as DTA). You'll found the wizard in payment order.
+
+""",
+ 'version': '1.0.1',
+ 'author': 'Camptocamp',
+ 'category': 'Localization',
+ 'website': 'http://www.camptocamp.com',
+ 'depends': ['base', 'account_payment', 'l10n_ch_base_bank', 'document'],
+ 'data': ["wizard/create_dta_view.xml",
+          "bank_view.xml"],
+ 'demo': ["demo/dta_demo.xml"],
+ 'test': ["test/l10n_ch_dta.yml"],
+ 'auto_install': False,
+ 'installable': True,
+ 'images': []
+ }
