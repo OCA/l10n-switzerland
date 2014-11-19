955d4d62645f40ddbf24a2ea54db8d3a2ce16333
Olivier Jossen <olivier.jossen@brain-tec.ch>
2014-11-19 10:42:31 +0100

    Ported l10n_ch_bank and l10n_ch_zip to version 8

---
+++ l10n_ch_bank/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Switzerland - Bank list',
+    'version': '8.0',
+    'author': 'brain-tec AG',
+    'category': 'Localisation',
+    'website': 'http://www.brain-tec.ch',
+    'summary': 'Banks names, addresses and BIC codes',
+    'description': """
+Swiss bank list
+===============
+This module will load all Swiss banks in OpenERP with their name,
+address and BIC code to ease the input of bank account.
+
+It is not mandatory to use OpenERP in Switzerland,
+but can improve the user experience.
+
+IMPORTANT:
+The module contains the newest bank data (21.10.2014).
+If you want to update all your banks, update via link 'Update Banks' in section 'Bank & Cash' under 'Settings/Configuration/Accounting'.
+
+""",
+    'depends': ['l10n_ch', 'l10n_ch_base_bank'],
+    'data': [
+              'bank.xml',
+              'res_config_view.xml'
+              ],
+    'images': [],
+    'demo': [],
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+}
