1144760e5a100de09d4bd2905ca4f056b7135365
Olivier Jossen <olivier.jossen@brain-tec.ch>
2014-11-20 10:16:21 +0100

    Corrected pylint errors

--- l10n_ch_zip/__init__.py
+++ l10n_ch_zip/__init__.py
@@ -19,6 +19,6 @@
 #
 ##############################################################################
 
-import better_zip
+from . import better_zip
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
