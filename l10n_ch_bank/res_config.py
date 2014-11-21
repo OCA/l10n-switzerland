2eb812217fae13ea58ab302a1f16d2f456e7e4a3
Olivier Jossen <olivier.jossen@brain-tec.ch>
2014-11-21 16:10:40 +0100

    Corrected PEP8 errors

--- l10n_ch_bank/res_config.py
+++ l10n_ch_bank/res_config.py
@@ -1,8 +1,7 @@
-# b-*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (c) 2010 brain-tec AG (http://www.brain-tec.ch)
-#    All Right Reserved
+#    Author: Nicolas Bessi. Copyright Camptocamp SA / Migrated to version 8 by brain-tec AG
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -26,41 +25,56 @@ from openerp.tools import misc
 import tempfile
 
 
-class base_config_settings(osv.osv_memory):
+class base_config_settings(osv.TransientModel):
     _inherit = 'account.config.settings'
-    
+
     def update_banks(self, cr, uid, ids, context=None):
-        # first update field 'noupdate' in ir.model.data for all entries related to model 'res.bank' and module 'l10n_ch' to 'False'
+        """ first update field 'noupdate' in ir.model.data for all entries
+        related to model 'res.bank' and module 'l10n_ch' to 'False' """
         sql = "update ir_model_data set noupdate = False where model = 'res.bank' and module = 'l10n_ch'"
         cr.execute(sql)
-         
-        # create new xml file in temp folder and set noupdate Flag to False in order to update all the entries when the customer wants to update all the banks
-        # set all bank's inactive which have an entry in ir_model_data -> this means they were created by xml
+
+        """ create new xml file in temp folder and set noupdate Flag to False in order to update all the entries when the customer wants to update all the banks
+        set all bank's inactive which have an entry in ir_model_data -> this
+        means they were created by xml """
         sql = "update res_bank set active = False where id in (select res_id from  ir_model_data where model = 'res.bank' and module = 'l10n_ch')"
         cr.execute(sql)
-        
+
         filename = 'bank.xml'
         noupdate = False
         pathname = os.path.join('l10n_ch_bank', filename)
         fp = misc.file_open(pathname)
         try:
-            # read current xml
+            " read current xml "
             xml_string = fp.read()
-            # create new xml to change noupdate 1 to 0
+            " create new xml to change noupdate 1 to 0 "
             new_bank_xml_path = tempfile.mkstemp('.xml')[1]
             new_bank_xml = open(new_bank_xml_path, 'w+')
-            new_bank_xml.write(xml_string.replace('data noupdate="1"', 'data noupdate="0"'))
+            new_bank_xml.write(
+                xml_string.replace(
+                    'data noupdate="1"',
+                    'data noupdate="0"'))
             new_bank_xml.close()
             fp = misc.file_open(new_bank_xml_path)
-            convert_xml_import(cr, 'l10n_ch_bank', fp, {}, 'update', noupdate, False)
-            #delete new_bank.xml file after all is done
+            convert_xml_import(
+                cr,
+                'l10n_ch_bank',
+                fp,
+                {},
+                'update',
+                noupdate,
+                False)
+            " delete new_bank.xml file after all is done "
             new_bank_xml.close()
             os.unlink(new_bank_xml_path)
         finally:
             fp.close()
         cr.commit()
 
-        # set field 'noupdate' in ir.model.data for all entries related to model 'res.bank' and module 'l10n_ch' back to 'True'
+        """ set field 'noupdate' in ir.model.data for all entries related to
+        model 'res.bank' and module 'l10n_ch' back to 'True' """
         sql = "update ir_model_data set noupdate = True where model = 'res.bank' and module = 'l10n_ch'"
         cr.execute(sql)
         return True
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
