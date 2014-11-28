4ccd409fb61d22123cd66658240790722b10ae13
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-11-28 15:31:20 +0100

    Better manifest spacing

--- l10n_ch_payment_slip/__openerp__.py
+++ l10n_ch_payment_slip/__openerp__.py
@@ -27,6 +27,7 @@ Swiss Payment slip known as ESR/BVR
 ===================================
 
 This addon allows you to print the ESR/BVR report Using Qweb report.
+
 The ESR/BVR is grenerated as an image and is availabe in a fields
 of the `l10n_ch.payment_slip` Model.
 
@@ -35,6 +36,7 @@ To modify it you have to cancel it and reconfirm the invoice.
 
 You can adjust the print out of ESR/BVR, which depend on each printer,
 for every company in the "BVR Data" tab.
+
 This is especialy useful when using pre-printed paper.
 An option also allow you to print the ESR/BVR in background when using
 white paper.
