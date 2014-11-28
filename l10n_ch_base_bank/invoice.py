46f83da9f6bc54576a8885515cc5503e1578eaeb
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-11-28 14:27:29 +0100

    Fix on change partner_id

--- l10n_ch_base_bank/invoice.py
+++ l10n_ch_base_bank/invoice.py
@@ -49,18 +49,19 @@ class AccountInvoice(models.Model):
                 bank_ids = user.company_id.partner_id.bank_ids
                 if bank_ids:
                     res['value']['partner_bank_id'] = bank_ids[0].id
+                    bank_id = bank_ids[0].id
         if partner_bank_id != bank_id:
             res['value']['partner_bank_id'] = bank_id
         return res
 
-    @api.multi
+    @api.onchange('partner_bank_id')
     def onchange_partner_bank(self, partner_bank_id=False):
         """update the reference invoice_type depending of the partner bank"""
-        result = super(AccountInvoice, self).onchange_partner_bank(
+        super(AccountInvoice, self).onchange_partner_bank(
             partner_bank_id=partner_bank_id
         )
-        if partner_bank_id:
-            partner_bank = self.env['res.partner.bank'].browse(partner_bank_id)
+        partner_bank = self.partner_bank_id
+        if partner_bank:
             if partner_bank.state == 'bvr':
                 result['value']['reference_type'] = 'bvr'
             else:
