f0c5844c952f2f52313c94748fddba9333f6b61b
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-12-01 09:19:05 +0100

    Migration of res.better.zip to new API

--- l10n_ch_zip/better_zip.py
+++ l10n_ch_zip/better_zip.py
@@ -18,34 +18,58 @@
 #
 ##############################################################################
 
-from openerp.osv import osv, fields
+from openerp import models, fields
 
 
-class res_better_zip_ext(osv.osv):
+class ResBetterZip(models.Model):
+    """ Inherit res.better.zip class in order to add swiss specific fields
 
-    " Inherit res.better.zip class in order to add swiss specific fields "
+    Fields from the original file downloaded from here:
+    https://match.post.ch/downloadCenter?product=2 -> File "PLZ Plus 1"
+
+    Documentation:
+    http://www.swisspost.ch/post-startseite/post-adress-services-match/post-direct-marketing-datengrundlage/post-match-zip-factsheet.pdf
+    """
+
     _inherit = 'res.better.zip'
 
-    """ fields from the original file downloaded from here:
-    https://match.post.ch/downloadCenter?product=2 -> File "PLZ Plus 1" """
-    _columns = {
-        'active': fields.boolean('Active'),
-        " Ordnungsnummer Post "
-        'onrp': fields.integer('Order number Post', size=5, help="Primary Key"),
-        'zip_type': fields.integer('Zip type', size=2),
-        " Zusatzziffer "
-        'additional_digit': fields.integer('Additional digit', size=2),
-        'lang': fields.integer('Sprachcode', size=1, help="1 = deutsch, 2 = französisch, 3 = italienisch, 4 = rätoromanisch"),
-        'lang2': fields.integer('Sprachcode abweichend', size=1, help="1 = deutsch, 2 = französisch, 3 = italienisch, 4 = rätoromanisch"),
-        'sort': fields.integer('Bestandeszugehörigkeit Sortierfile', size=1),
-        'post_delivery_through': fields.integer('Briefzustellung durch', size=5),
-        " Gemeindenummer BfS "
-        'communitynumber_bfs': fields.integer('Community number BfS', size=5),
-        'valid_from': fields.char('Valid from', size=8, help="YYYYMMDD"),
-    }
-
-    _defaults = {
-        'active': lambda *a: 1,
-    }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    active = fields.Boolean(string='Active', default=True)
+    onrp = fields.Char(string='Swiss Post classification no. (ONRP)',
+                       size=5, help="Primary Key")
+    zip_type = fields.Char(string='Postcode type', size=2)
+    additional_digit = fields.Char(string='Additional poscode digits', size=2)
+    lang = fields.Char(
+        string='Language code', size=1,
+        help="Language (language majority) within a postcode area. "
+             "1 = German, 2 = French, 3 = Italian, 4 = Romansh. "
+             "For multi-lingual localities, the main language is indicated.",
+    )
+    lang2 = fields.Char(
+        'Alternative language code',
+        size=1,
+        help="Additional language within a postcode.\n"
+             "One alternative language code may appear for each postcode.\n"
+             "1 = German, 2 = French, 3 = Italian, 4 = Romansh.",
+    )
+    sort = fields.Boolean(
+        string='Present in sort file',
+        help="Indicates if the postcode is included in the «sort file»"
+             "(MAT[CH]sort): 0 = not included, 1 = included. "
+             "Delivery information with addresses (only postcode and "
+             "streets) are published in the sort file.",
+    )
+    post_delivery_through = fields.Integer(
+        string='Mail delivery by',
+        size=5,
+        help="Indicates the post office (ONRP) that delivers most of the "
+             "letters to the postcode addresses. This information can be "
+             "used for bag addresses too."
+    )
+    communitynumber_bfs = fields.Integer(
+        string='FSO municipality number (BFSNR)',
+        size=5,
+        help="Numbering used by the Federal Statistical Office for "
+             "municipalities in Switzerland and the Principality of "
+             "Liechtenstein",
+    )
+    valid_from = fields.Char(string='Valid from', size=8)
