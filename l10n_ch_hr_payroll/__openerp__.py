4f25f8b54bb9e006f22feb937b5dfb2812a40845
Sébastien Gendre <sge@open-net.ch>
2014-12-08 12:30:01 +0100

    Remove description from l10n_ch_hr_payroll/__openerp__.py file.

--- l10n_ch_hr_payroll/__openerp__.py
+++ l10n_ch_hr_payroll/__openerp__.py
@@ -33,41 +33,6 @@
     'author': 'Open-Net Sàrl',
     'depends': ['decimal_precision', 'hr_payroll', 'hr_payroll_account'],
     'version': '1.0.1',
-    'description': """
-Swizerland Payroll Rules.
-=========================
-
-**Features list :**
-    * Add Swiss salary rule categories
-    * Add Swiss salary rules
-    * Add children in school to employee
-    * Add LPP range to contract
-
-**For functionnal information:**
-http://ur1.ca/ir5ou
-
-**Author :**
-Open Net Sàrl -Industrie 59 1030 Bussigny Suisse - http://www.open-net.ch
-
-**Contact :**
-info@open-net.ch
-
-**History :**
-
-V1.0.0: 2014-11-07/Sge
-    * Add Salary rule categories
-    * Add Salary rules
-    * Add Employee children in school
-    * Add Contract LPP rate
-
-V1.0.1: 2014-11-11/Sge
-    * Set the 'LPP rate'' digits to 'Payroll Rate' decimal accuracy
-
-V1.0.2:
-    * Add some minor changes, based on pull request #66 comments.
-
-    """,
-
     'auto_install': False,
     'demo': [],
     'website': 'http://open-net.ch',
