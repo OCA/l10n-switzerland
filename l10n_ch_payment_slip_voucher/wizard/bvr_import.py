2b8df3eb16b7214d655a4d70ed3abfe88d037303
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-12-13 21:51:48 +0100

    [ADD] not working tests

--- l10n_ch_payment_slip_voucher/wizard/bvr_import.py
+++ l10n_ch_payment_slip_voucher/wizard/bvr_import.py
@@ -21,6 +21,8 @@
 
 from openerp import models, fields, exceptions, api
 import base64
+import time
+from openerp.tools.translate import _
 
 
 class BvrImporterWizard(models.TransientModel):
@@ -32,9 +34,8 @@ class BvrImporterWizard(models.TransientModel):
     currency_id = fields.Many2one('res.currency', "Currency", required=True) # TODO default
 
     def _build_voucher_header(self, partner_id, record):
-        voucher_obj = self.env['account.voucher']
         date = record['date'] or time.strftime('%Y-%m-%d')
-        voucher_vals {
+        voucher_vals = {
             'type': 'receipt',
             'name': record['reference'],
             'partner_id': partner_id,
@@ -45,9 +46,11 @@ class BvrImporterWizard(models.TransientModel):
             'date': date,
             'amount': abs(record['amount']),
             }
+        return voucher_vals
 
     def _build_voucher_lines(self, partner_id, record, voucher_id):
         voucher_obj = self.env['account.voucher']
+        date = fields.Date.today()
         result = voucher_obj.onchange_partner_id(self.env.cr, self.env.uid, [],
                                                  partner_id,
                                                  self.journal_id.id,
@@ -57,28 +60,37 @@ class BvrImporterWizard(models.TransientModel):
                                                  date,
                                                  context=self.env.context)
         voucher_line_dict = False
+        move_line_obj = self.env['account.move.line']
         if result['value']['line_cr_ids']:
             for line_dict in result['value']['line_cr_ids']:
                 move_line = move_line_obj.browse(
-                    cursor, uid, line_dict['move_line_id'], context)
-                if move_id == move_line.move_id.id:
+                    self.env.cr, self.env.uid, line_dict['move_line_id'],
+                    self.env.context)
+                if move_line.transaction_ref == record['reference']:
                     voucher_line_dict = line_dict
         if voucher_line_dict:
             voucher_line_dict.update({'voucher_id': voucher_id})
-            voucher_line_obj.create(
-                cursor, uid, voucher_line_dict, context=context)
+        return voucher_line_dict
 
-    def _search_move_lines(self, record):
+    def get_partner_from_ref(self, reference):
         move_line_obj = self.env['account.move.line']
-        reference = record['reference']
-        line_ids = move_line_obj.search(
+        partner_id = False
+        lines = move_line_obj.search(
             [('transaction_ref', '=', reference),
              ('reconcile_id', '=', False),
              ('account_id.type', 'in', ['receivable', 'payable']),
              ('journal_id.type', '=', 'sale')],
             order='date desc',
         )
-        return line_ids
+        for line in lines:
+            if partner_id and line.partner_id.id != partner_id:
+                raise exceptions.Warning(
+                    "Too many partners related to reference %s" % reference)
+            partner_id = line.partner_id.id
+        if not partner_id:
+            raise exceptions.Warning(
+                "Can't find a partner for reference %s" % reference)
+        return partner_id
 
     def _import_v11(self):
         """Import v11 file and transfor it into statement lines
@@ -88,6 +100,8 @@ class BvrImporterWizard(models.TransientModel):
         """
         attachment_obj = self.env['ir.attachment']
         v11_wizard = self.env['v11.import.wizard']
+        voucher_obj = self.env['account.voucher']
+        voucher_line_obj = self.env['account.voucher.line']
         v11file = self.v11file
         if not v11file:
             raise exceptions.Warning(
@@ -101,25 +115,41 @@ class BvrImporterWizard(models.TransientModel):
                   'it contains invalid caracter %s'),
                 repr(decode_err)
             )
-        records = v11_wizard._parse_lines(lines)
-        move_lines = []
+        std_importer = v11_wizard.create({
+            'v11file': self.v11file,
+            'total_cost': self.total_cost,
+            'total_amount': self.total_amount,
+            })
+        records = std_importer._parse_lines(lines)
+        voucher_ids = []
         for record in records:
-            move_lines.append(self._search_move_lines(record))
-        attachment_obj.create(
-            {
-                'name': 'V11 %s' % time.strftime(
-                    "%Y-%m-%d_%H:%M:%S", time.gmtime()
-                ),
-                'datas': self.v11file,
-                'datas_fname': 'BVR %s.txt' % time.strftime(
-                    "%Y-%m-%d_%H:%M:%S", time.gmtime()
-                ),
-                'res_model': 'account.bank.statement',
-                'res_id': statement.id,
-            },
-        )
+            partner_id = self.get_partner_from_ref(record['reference'])
+            voucher = voucher_obj.create(
+                self._build_voucher_header(partner_id, record))
+            voucher_line_obj.create(
+                self._build_voucher_lines(partner_id, record, voucher.id))
+            voucher_ids.append(voucher.id)
+        for voucher_id in voucher_ids:
+            attachment_obj.create(
+                {
+                    'name': 'V11 %s' % time.strftime(
+                        "%Y-%m-%d_%H:%M:%S", time.gmtime()
+                    ),
+                    'datas': self.v11file,
+                    'datas_fname': 'BVR %s.txt' % time.strftime(
+                        "%Y-%m-%d_%H:%M:%S", time.gmtime()
+                    ),
+                    'res_model': 'account.voucher',
+                    'res_id': voucher_id,
+                },
+            )
 
-        return {}
+        action_res = self.env['ir.actions.act_window'].for_xml_id(
+            'account_voucher', 'action_vendor_receipt')
+        action_res[
+            'domain'
+        ] = "[('id','in', ["+','.join(map(str, voucher_ids))+"])]"
+        return action_res
 
     @api.multi
     def import_v11(self):
