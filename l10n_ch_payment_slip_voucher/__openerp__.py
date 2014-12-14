69da4e358180bf832179d1800a0ca9625c6aefa7
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-12-14 13:16:33 +0100

    [FIX] description

--- l10n_ch_payment_slip_voucher/__openerp__.py
+++ l10n_ch_payment_slip_voucher/__openerp__.py
@@ -25,7 +25,7 @@
 This module allows you to import v11 files provided
 by financial institute into a payment voucher
 
-To do so, use the wizard provided under 'bank and cash'.
+To do so, use the wizard provided under Accounting -> Customers.
 """,
  'version': '1.0',
  'author': 'Agile Business Group',
