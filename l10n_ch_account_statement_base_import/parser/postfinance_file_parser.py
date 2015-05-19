de1005ce0c9abb01a9922bf76bbd500a270cce5c
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2015-05-19 19:30:50 +0200

    Port of postfinance XML file into bank statements

--- l10n_ch_account_statement_base_import/parser/postfinance_file_parser.py
+++ l10n_ch_account_statement_base_import/parser/postfinance_file_parser.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright David Wulliamoz SA
+#    Author: Nicolas Bessi, David Wulliamoz
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -16,140 +16,242 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp.tools.translate import _
-from openerp.osv.osv import except_osv
-import tempfile
 import datetime
-import tarfile
-from account_statement_base_import.parser.parser \
-    import BankStatementImportParser
-
+import re
+from tarfile import TarFile, TarError
+from cStringIO import StringIO
 from lxml import etree
-
 import logging
+
+from openerp import fields
+
+from .base_parser import BaseSwissParser
 
 _logger = logging.getLogger(__name__)
 
 
-class XMLPFParser(BankStatementImportParser):
+class XMLPFParser(BaseSwissParser):
     """
     Parser for XML Postfinance Statements (can be wrapped in a tar.gz file)
     """
 
-    def __init__(self, parse_name, ftype='xml', **kwargs):
-        """
-        :param char: parse_name: The name of the parser
-        :param char: ftype: extension of the file (could be xml)
-        """
+    _ftype = 'postfinance'
 
-        super(XMLPFParser, self).__init__(parse_name, **kwargs)
-
-        if ftype in ('xml', 'gz'):
-            self.ftype = ftype
-        else:
-            raise except_osv(_('User Error'),
-                             _('Invalid file type %s. Please use xml or gz') %
-                             ftype)
-
-    @classmethod
-    def parser_for(cls, parser_name):
+    def __init__(self, data_file):
+        """Constructor
+        Try to uncompress the file if possible
         """
-        Used by the new_bank_statement_parser class factory. Return true if
-        the providen name is generic_csvxls_so
-        """
-        return parser_name == 'pf_xmlparser'
+        super(XMLPFParser, self).__init__(data_file)
+        self.data_file = self._get_content_from_stream(data_file)
 
-    def _custom_format(self, *args, **kwargs):
-        """
-        Extract the XML statement if the given file is a tar gz file.
-        """
-        if self.ftype == 'gz':
-            pf_file = tempfile.NamedTemporaryFile()
-            pf_file.write(self.filebuffer)
-            # We ensure that cursor is at beginning of file
-            pf_file.seek(0)
+    def _get_content_from_stream(self, data_file):
+        """Source file can be a raw or tar file. We try to guess the
+        file type and return valid file content
 
-            tfile = tarfile.open(fileobj=pf_file, mode="r:gz")
-            tfile_xml = [filename
-                         for filename in tfile.getnames()
-                         if filename.endswith('.xml')]
-
-            self.filebuffer = tfile.extractfile(tfile_xml[0]).read()
-
-        return True
-
-    def _pre(self, *args, **kwargs):
+        :retrun: uncompressed file content
+        :rtype: string
         """
-        No pre-treatment needed for this parser.
-        """
-        return True
+        # https://hg.python.org/cpython/file/6969bac411fa/Lib/tarfile.py#l2605
+        pf_file = StringIO(data_file)
+        pf_file.seek(0)
+        try:
+            # Taken from original code it seem we only want to
+            # parse first XML file. TO BE CONFIRMED
+            tar_file = TarFile.open(fileobj=pf_file, mode="r:gz")
+            xmls = [tar_content
+                    for tar_content in tar_file.getnames()
+                    if tar_content.endswith('.xml')]
+            return tar_file.extractfile(xmls[0]).read()
+        except TarError:
+            return data_file
 
-    def _parse(self, *args, **kwargs):
+    def ftype(self):
+        """Gives the type of file we want to import
+
+        :return: imported file type
+        :rtype: string
         """
-        Launch the parsing through .xml
+        return super(XMLPFParser, self).ftype()
+
+    def get_currency(self):
+        """Returns the ISO currency code of the parsed file
+
+        :return: The ISO currency code of the parsed file eg: CHF
+        :rtype: string
         """
-        reader = etree.fromstring(self.filebuffer)
-        r = reader.xpath("//SG5/MOA/C516")
-        for move in r:
+        return super(XMLPFParser, self).get_currency()
+
+    def get_account_number(self):
+        """Return the account_number related to parsed file
+
+        :return: The account number of the parsed file
+        :rtype: string
+        """
+        return super(XMLPFParser, self).get_account_number()
+
+    def get_statements(self):
+        """Return the list of bank statement dict.
+         Bank statements data: list of dict containing (optional items marked by o) :
+            - 'name': string (e.g: '000000123')
+            - 'date': date (e.g: 2013-06-26)
+            -o 'balance_start': float (e.g: 8368.56)
+            -o 'balance_end_real': float (e.g: 8888.88)
+            - 'transactions': list of dict containing :
+                - 'name': string (e.g: 'KBC-INVESTERINGSKREDIET 787-5562831-01')
+                - 'date': date
+                - 'amount': float
+                - 'unique_import_id': string
+                -o 'account_number': string
+                    Will be used to find/create the res.partner.bank in odoo
+                -o 'note': string
+                -o 'partner_name': string
+                -o 'ref': string
+
+        :return: a list of statement
+        :rtype: list
+        """
+        return super(XMLPFParser, self).get_statements()
+
+    def file_is_known(self):
+        """Predicate the tells if the parser can parse the data file
+
+        :return: True if file is supported
+        :rtype: bool
+        """
+        try:
+            return re.search(r'\<IC\b', self.data_file) is not None
+        except:
+            return False
+
+    def _parse_account_number(self, tree):
+        """Parse file account number using xml tree
+        :param tree: lxml element tree instance
+        :type tree: :py:class:`lxml.etree.element.Element`
+
+        :return: the file account number
+        :rtype: string
+        """
+        account_node = tree.xpath('//SG2/FII/C078/D_3194/text()')
+        if not account_node:
+            return
+        if len(account_node) != 1:
+            raise ValueError('Many account found for postfinance statement')
+        return account_node[0]
+
+    def _parse_currency_code(self, tree):
+        """Parse file currency ISO code using xml tree
+        :param tree: lxml element tree instance
+        :type tree: :py:class:`lxml.etree.element.Element`
+
+        :return: the currency ISO code of the file eg: CHF
+        :rtype: string
+        """
+        currency_node = tree.xpath('//SG2/FII/C078/D_6345/@Value')
+        if not currency_node:
+            return
+        if len(currency_node) != 1:
+            raise ValueError('Many currency found for postfinance statement')
+        return currency_node[0]
+
+    def _parse_statement_balance(self, tree):
+        """Parse file start and end balance
+        :param tree: lxml element tree instance
+        :type tree: :py:class:`lxml.etree.element.Element`
+
+        :return: the file start and end balance
+        :rtype: tuple (start, end) balances
+        """
+        balance_start = balance_end = False
+        balance_nodes = tree.xpath("//SG5/MOA/C516")
+        for move in balance_nodes:
             if move.xpath(".//@Value='315'"):
-                self.balance_start = float(move.xpath("./D_5004/text()")[0])
+                balance_start = float(move.xpath("./D_5004/text()")[0])
 
             if move.xpath(".//@Value='343'"):
-                self.balance_end = float(move.xpath("./D_5004/text()")[0])
+                balance_end = float(move.xpath("./D_5004/text()")[0])
+        return balance_start, balance_end
 
-        r = reader.xpath("//SG6")
-        res = []
-        for move in r:
-            if move.xpath(".//@Value='TGT'"):
-                transaction_date = move.xpath("DTM/C507/D_2380/text()")
-                if transaction_date:
+    def _parse_transactions(self, tree):
+        """Parse bank statement lines from file
+        list of dict containing :
+            - 'name': string (e.g: 'KBC-INVESTERINGSKREDIET 787-5562831-01')
+            - 'date': date
+            - 'amount': float
+            - 'unique_import_id': string
+            -o 'account_number': string
+                Will be used to find/create the res.partner.bank in odoo
+            -o 'note': string
+            -o 'partner_name': string
+            -o 'ref': string
+
+        :param tree: lxml element tree instance
+        :type tree: :py:class:`lxml.etree.element.Element`
+
+        :return: a list of transactions
+        :rtype: list
+        """
+        transactions = []
+        transaction_nodes = tree.xpath("//SG6")
+        for transaction in transaction_nodes:
+            res = {}
+            desc = '/'
+            date = datetime.date.today()
+            if transaction.xpath(".//@Value='TGT'"):
+                transaction_date_text = transaction.xpath(
+                    "DTM/C507/D_2380/text()"
+                )
+                if transaction_date_text:
                     date = datetime.datetime.strptime(
-                        transaction_date[0], "%Y%m%d").date()
-                if move.xpath(".//@Value='ZZZ'"):
-                    ref = move.xpath("RFF/C506/D_1154/text()")[1]
-                else:
-                    ref = "/"
-                lib = "\n".join(move.xpath("FTX/C108/D_4440/text()"))
-                amount = float(move.xpath("MOA/C516/D_5004/text()")[0])
-                if move.xpath("MOA/C516/D_5025/@Value='211'"):
-                    amount *= -1
+                        transaction_date_text[0], "%Y%m%d").date()
+            res['date'] = fields.Date.to_string(date)
 
-                line_res = {'ref': ref, 'lib': lib, 'amount': amount}
-                if date:
-                    line_res.update({'date': date.strftime(
-                        "%Y-%m-%d")})
-                res.append(line_res)
+            if transaction.xpath(".//@Value='ZZZ'"):
+                desc = transaction.xpath("RFF/C506/D_1154/text()")[1]
 
-        self.result_row_list = res
+            res['ref'] = "\n".join(transaction.xpath("FTX/C108/D_4440/text()"))
+            amount = float(transaction.xpath("MOA/C516/D_5004/text()")[0])
+            if transaction.xpath("MOA/C516/D_5025/@Value='211'"):
+                amount *= -1
+            res['amount'] = amount
+            # We have an issue with XPATH and namespace here because of
+            # faulty definition on a deprecated URL
+            uid = [x.text for x in transaction.iter()
+                   if (x.prefix == 'PF' and x.tag.endswith('D_4754'))]
+            uid = uid[0] if uid else None
+            res['unique_import_id'] = uid
+            res['name'] = uid if uid else desc
+            res['account_number'] = None
+            res['note'] = None
+            res['partner_name'] = None
+            transactions.append(res)
+        return transactions
+
+    def _parse_statement_date(self, tree):
+        """Parse file statement date from tree
+        :param tree: lxml element tree instance
+        :type tree: :py:class:`lxml.etree.element.Element`
+
+        :return: A date usable by Odoo in write or create dict
+        :rtype: string
+        """
+        # I was not able to find a correct segment group to extract the date
+        date = datetime.date.today()
+        return fields.Date.to_string(date)
+
+    def _parse(self):
+        """
+        Launch the parsing through The XML file. It sets the various
+        property of the class from the parse result.
+        This implementation expect one XML file to represents one statement
+        """
+        tree = etree.fromstring(self.data_file)
+        self.currency_code = self._parse_currency_code(tree)
+        self.account_number = self._parse_account_number(tree)
+        statement = {}
+        balance_start, balance_stop = self._parse_statement_balance(tree)
+        statement['balance_start'] = balance_start
+        statement['balance_end_real'] = balance_stop
+        statement['date'] = self._parse_statement_date(tree)
+        statement['transactions'] = self._parse_transactions(tree)
+        self.statements.append(statement)
         return True
-
-    def _validate(self, *args, **kwargs):
-        """
-        Nothing to validate.
-        """
-        return True
-
-    def _post(self, *args, **kwargs):
-        """
-        No special conversion needed.
-        """
-        return True
-
-    def get_st_line_vals(self, line, *args, **kwargs):
-        """
-        This method must return a dict of vals that can be passed to create
-        method of statement line in order to record it.
-            :param:  line: a dict of vals that represent a line of
-                           result_row_list
-            :return: dict of values to give to the create method of
-                     statement line,
-        """
-        res = {
-            'name': line.get('lib', line.get('ref', '/')),
-            'date': line.get('date', datetime.date.today()),
-            'amount': line.get('amount', 0.0),
-            'ref': line.get('ref', '/'),
-            'label': line.get('lib', ''),
-        }
-
-        return res
