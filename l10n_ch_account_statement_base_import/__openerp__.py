99e850b5c06b41f2efe4f86e455e1969ba8cbb65
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2015-06-09 17:44:02 +0200

    Add support of scan attachement on statement import
    This is a temporary solution that should be deprecated by
    https://github.com/OCA/bank-statement-import/pull/21

--- l10n_ch_account_statement_base_import/__openerp__.py
+++ l10n_ch_account_statement_base_import/__openerp__.py
@@ -31,7 +31,7 @@
      'python': ['xlrd'],
  },
  'website': 'http://www.compassion.ch/',
- 'data': [],
+ 'data': ['account_bank_statement_import_view.xml'],
  'test': [],
  'installable': True,
  'images': [],
