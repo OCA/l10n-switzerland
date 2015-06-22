3216883e1e81b4752f1bfb7ceb4884ebffa7568d
unknown <mn@copado.de>
2015-06-22 12:26:53 +0200

    [ADD] Country States for Switzerland

---
+++ l10n_ch_states/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Mathias Neef
+#    Copyright 2015 copadoMEDIA UG
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Switzerland Country States',
+    'category': 'Localisation',
+    'summary': '',
+    'version': '1.0',
+    'author': 'copado MEDIA UG, Odoo Community Association (OCA)',
+    'website': 'http://www.copado.de',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'data': [],
+    'update_xml': ['data/res_country_states.xml'],
+    'demo': [],
+    'installable': True,
+    'application': False,
+}
