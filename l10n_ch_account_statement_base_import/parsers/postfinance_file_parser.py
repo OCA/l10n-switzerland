4962680fd50015f7811fe654ae38221c344e494d
Emmanuel Mathier <emmanuel.mathier@gmail.com>
2015-09-11 14:59:34 +0200

    s/attachment/attachments in postfinance_file_parser.py

--- l10n_ch_account_statement_base_import/parsers/postfinance_file_parser.py
+++ l10n_ch_account_statement_base_import/parsers/postfinance_file_parser.py
@@ -82,7 +82,7 @@ class XMLPFParser(BaseSwissParser):
         :param data_file: raw statement file sent to odoo (not in b64)
         :type data_file: basestring subclass
 
-        :return: Return a dict containing all attachment ready
+        :return: Return a dict containing all attachments ready
         to be saved in Odoo.
         """
         pf_file = StringIO(data_file)
@@ -265,11 +265,11 @@ class XMLPFParser(BaseSwissParser):
         return transactions
 
     def _parse_attachments(self, tree):
-        """Parse file statement to get wich attachement to use
+        """Parse file statement to get wich attachment to use
         :param tree: lxml element tree instance
         :type tree: :py:class:`lxml.etree.element.Element`
 
-        :return: a list of attachement tuple (name, content)
+        :return: a list of attachment tuple (name, content)
         :rtype: list
         """
         attachments = [('xml statment', self.data_file.encode('base64'))]
