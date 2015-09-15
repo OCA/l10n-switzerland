d4eaf748ed43e3db5e6e4ff7af33880da4ab829f
Emmanuel Mathier <emmanuel.mathier@gmail.com>
2015-09-15 15:47:04 +0200

    Fix PEP8

--- l10n_ch_account_statement_base_import/parsers/v11_file_parser.py
+++ l10n_ch_account_statement_base_import/parsers/v11_file_parser.py
@@ -19,7 +19,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import datetime
 import time
 import logging
 import uuid
