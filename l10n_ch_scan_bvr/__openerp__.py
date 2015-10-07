51d9129fb0271601c9b8dc34ac522e1ff54fc2d7
Alex Comba <alex.comba@agilebg.com>
2015-10-07 15:56:34 +0200

    Remove useless dependencies

--- l10n_ch_scan_bvr/__openerp__.py
+++ l10n_ch_scan_bvr/__openerp__.py
@@ -4,17 +4,19 @@
 # (c) 2015 Alex Comba - Agile Business Group
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-{"name": "Switzerland - Scan ESR/BVR to create invoices",
- "version": "8.0.1.0.0",
- "author": "Camptocamp, Odoo Community Association (OCA)",
- "category": "Generic Modules/Others",
- "website": "http://www.camptocamp.com",
- "license": "AGPL-3",
- "depends": ["l10n_ch",
-             "l10n_ch_payment_slip",
-             "account"],
- "data": ["wizard/scan_bvr_view.xml",
-          "views/partner_view.xml",
-          "views/bank_view.xml"],
- 'installable': True,
- }
+{
+    "name": "Switzerland - Scan ESR/BVR to create invoices",
+    "version": "8.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "category": "Generic Modules/Others",
+    "website": "http://www.camptocamp.com",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_ch_payment_slip",
+    ],
+    "data": [
+        "wizard/scan_bvr_view.xml",
+        "views/partner_view.xml",
+        "views/bank_view.xml"],
+    'installable': True,
+}
