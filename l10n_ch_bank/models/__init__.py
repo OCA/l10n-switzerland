eacf2a2fae9d5a309f396d800c58bf2197b1a87f
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-10-21 14:24:18 +0200

    [PORT] l10n_ch_bank to 9.0 - Update README.rst - move files in folders models and data

--- l10n_ch_bank/models/__init__.py
+++ l10n_ch_bank/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2011-2014 Nicolas Bessi (Camptocamp SA)
 # © 2014 Olivier Jossen brain-tec AG
-# © 2015 Yannick Vaucher (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import models
+from . import bank
+from . import res_config
