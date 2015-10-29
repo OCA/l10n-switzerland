f51170e7e138b7e91a3f0a224ed184dd08a4a530
Paulius Sladkevičius <paulius@hbee.eu>
2015-10-29 14:42:27 +0200

    Print bank address instead company partner address

--- l10n_ch_payment_slip/bank.py
+++ l10n_ch_payment_slip/bank.py
@@ -17,7 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class ResPartnerBank(models.Model):
@@ -33,3 +33,23 @@ class ResPartnerBank(models.Model):
     print_bank = fields.Boolean('Print Bank on BVR')
     print_account = fields.Boolean('Print Account Number on BVR')
     print_partner = fields.Boolean('Print Partner Address on BVR')
+
+    @api.multi
+    def _display_address(self):
+        """ Format bank address """
+        self.ensure_one()
+        address_format = (
+            self.country_id.address_format or
+            "%(company_name)s\n%(street)s\n%(city)s %(state_code)s"
+            " %(zip)s\n%(country_name)s")
+        args = {
+            'state_code': self.state_id.code or '',
+            'state_name': self.state_id.name or '',
+            'country_code': self.country_id.code or '',
+            'country_name': self.country_id.name or '',
+            'company_name': self.owner_name or '',
+            'street2': '',  # No such field on the this model
+        }
+        for field in ('street', 'zip', 'city', 'state_id', 'country_id'):
+            args[field] = getattr(self, field) or ''
+        return address_format % args
