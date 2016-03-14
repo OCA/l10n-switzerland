2968cb484ee0d7a15fbe6c1dff5abe30029ae3ae
Open Net Sàrl <info@open-net.ch>
2016-03-14 16:38:26 +0100

    Fix PEP8

--- l10n_ch_hr_payroll/models/hr_payslip.py
+++ l10n_ch_hr_payroll/models/hr_payslip.py
@@ -148,4 +148,3 @@ set slip_id=%d where id in (%s)""" % (payslip.id, ','.join(move_line_ids))
         for expense in expenses:
             expenses.state = 'done'
         return super(HrPayslip, self).process_sheet()
-        
