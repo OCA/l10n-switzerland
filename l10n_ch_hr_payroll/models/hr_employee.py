057f2de7775998350eabeec430a56585c3fa911a
David Coninckx <dco@open-net.ch>
2016-03-15 08:31:49 +0100

    Short header on __init__.py, hr_contract.py, hr_employee.py

--- l10n_ch_hr_payroll/models/hr_employee.py
+++ l10n_ch_hr_payroll/models/hr_employee.py
@@ -1,30 +1,6 @@
 # -*- coding: utf-8 -*-
-#
-#  File: models/hr_employee.py
-#  Module: l10n_ch_hr_payroll
-#
-#  Created by cyp@open-net.ch
-#
-#  Copyright (c) 2014-TODAY Open-Net Ltd.
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2012 David Coninckx (Opennet Sarl)
+# License into __openerp__.py.
 
 
 from openerp import fields, models
