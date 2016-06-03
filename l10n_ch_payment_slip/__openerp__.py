2fe0478ff1c85a9a39db91e96c2367b35de074c7
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-06-03 15:47:59 +0200

    Bump module version and add contributor

--- l10n_ch_payment_slip/__openerp__.py
+++ l10n_ch_payment_slip/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {'name': 'Switzerland - Payment Slip (BVR/ESR)',
  'summary': 'Print ESR/BVR payment slip with your invoices',
- 'version': '9.0.2.1.0',
+ 'version': '9.0.2.1.1',
  'author': "Camptocamp,Odoo Community Association (OCA)",
  'category': 'Localization',
  'website': 'http://www.camptocamp.com',
