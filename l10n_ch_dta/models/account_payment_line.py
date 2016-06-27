8bc4e127dbfb4aafba7fa716cd9b39eea5f3552b
Kumar Aberer <kumar.aberer@braintec-group.com>
2016-06-27 16:10:13 +0200

    fixing lints

--- l10n_ch_dta/models/account_payment_line.py
+++ l10n_ch_dta/models/account_payment_line.py
@@ -2,7 +2,7 @@
 # © 2015-2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, _
+from openerp import models, fields
 
 
 class AccountPaymentLine(models.Model):
