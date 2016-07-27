75d3935507c8d355345753c1a4b2e75f17934258
Louis Bettens <lbe@open-net.ch>
2016-07-27 14:26:43 +0200

    add full test case
    It's heavy, but it's exhaustive and it allows me to gauge at a glance
    how future changes to the module I'll be making affect the output.

--- l10n_ch_import_winbiz/tests/test_import.py
+++ l10n_ch_import_winbiz/tests/test_import.py
@@ -3,9 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import openerp.tests.common as common
-from openerp.modules import get_module_resource
+from openerp.modules import get_resource_path
+import logging
 from StringIO import StringIO
 import base64
+from pprint import pprint
+import difflib
+import tempfile
+import os
+
+_logger = logging.getLogger(__name__)
 
 
 class TestImport(common.TransactionCase):
@@ -19,33 +26,41 @@ class TestImport(common.TransactionCase):
 
         user_type = self.env['account.account.type'].search(
             [('include_initial_balance', '=', False)], limit=1)
+
+        self.account_codes = {}
         for code in ['1000', '1010', '1020', '1050', '1061', '1120', '2000',
                      '2010', '2091', '2200', '4000', '4051', '4055', '4095',
                      '4210', '4300', '4400', '4412', '4510', '4511', '4590',
                      '4600', '5472', '5700', '6200', '6207', '8100']:
-            found = account_obj.search([('code', '=', code)])
-            if found:  # patch it within the transaction
-                found.user_type_id = user_type.id
+            acc = account_obj.search([('code', '=', code)])
+            if acc:  # patch it within the transaction
+                acc.user_type_id = user_type.id
             else:
-                account_obj.create({
+                acc = account_obj.create({
                     'name': 'dummy %s' % code,
                     'code': code,
                     'user_type_id': user_type.id,
                     'reconcile': True})
+            self.account_codes[acc.id] = acc.code
 
     def test_import(self):
         journal_obj = self.env['account.journal']
         misc = journal_obj.search(
             [('name', 'ilike', 'miscellaneous')], limit=1)
 
-        test_file_path = get_module_resource('l10n_ch_import_winbiz',
-                                             'tests',
-                                             'winbiz.xls')
+        def get_path(filename):
+            res = get_resource_path('l10n_ch_import_winbiz', 'tests', filename)
+            return res
+        input = open(get_path('input.xls'))
+        gold = open(get_path('golden-output.txt'))
+        temp = tempfile.NamedTemporaryFile(prefix='odoo-l10n_ch_import_winbiz',
+                                           delete=False)
+
         buf = StringIO()
-        with open(test_file_path) as f:
-            base64.encode(f, buf)
+        base64.encode(input, buf)
         contents = buf.getvalue()
         buf.close()
+
         wizard = self.env['account.winbiz.import'].create({
             'journal_id': misc.id,
             'file': contents})
@@ -53,3 +68,20 @@ class TestImport(common.TransactionCase):
 
         res = wizard.imported_move_ids
         res.assert_balanced()
+
+        # Get a predictable representation that can be compared across runs
+        data = res.copy_data()
+        for mv in data:
+            del mv['journal_id']
+            for _, _, ln in mv['line_ids']:
+                del ln['move_id']
+                ln['account_id'] = self.account_codes[ln['account_id']]
+        pprint(data, temp)
+        temp.seek(0)
+        diff = list(difflib.unified_diff(gold.readlines(), temp.readlines(),
+                                         gold.name,        temp.name))
+        if len(diff) > 2:
+            for i in diff:
+                _logger.error(i)
+            self.fail("actual output doesn't match exptected output")
+        os.remove(temp.name) # intentionally keep it in case of test failure
