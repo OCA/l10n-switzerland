b8f295f6f1702a1a0b471b8dfceb0e6fb8fe76fa
Louis Bettens <lbe@open-net.ch>
2016-09-23 16:32:45 +0200

    update copyright

--- l10n_ch_import_cresus/tests/__init__.py
+++ l10n_ch_import_cresus/tests/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Bettens Louis (Open Net Sarl)
+# Copyright 2016 Open Net Sàrl
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import test_import
