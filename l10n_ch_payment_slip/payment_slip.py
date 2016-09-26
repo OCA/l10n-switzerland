a0cd53f4326d80b3316dda350f11db3fceb570c1
Johann Bélet <j.belet@juratronic.ch>
2016-09-26 06:40:32 +0200

    Updated zip/city line generation Updated tabs to spaces

--- l10n_ch_payment_slip/payment_slip.py
+++ l10n_ch_payment_slip/payment_slip.py
@@ -564,8 +564,11 @@ class PaymentSlip(models.Model):
             if not line:
                 continue
             text.textLine(line)
-        line = (bank.zip + '' or '') + ' ' + (bank.city or '')
+        
+        line = [str(bank.zip or ''), bank.city]
+        line = ' '.join([s for s in line if s])
         text.textLine(line)
+        
         canvas.drawText(text)
 
     @api.model
