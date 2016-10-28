2b63cf552ce61f8c3e114b4a8286e1d4288518cf
Ermin Trevisan <trevi@twanda.com>
2016-10-28 18:39:07 +0200

    Add files via upload

--- l10n_ch_hr_payroll/__openerp__.py
+++ l10n_ch_hr_payroll/__openerp__.py
@@ -21,11 +21,12 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
+#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
+#
+#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Switzerland - Payroll',
