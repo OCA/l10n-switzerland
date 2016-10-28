fb3e50cf47b417b4c6726827a5e9c083204fc8a9
Ermin Trevisan <trevi@twanda.com>
2016-10-28 14:52:25 +0200

    Add files via upload

--- l10n_ch_hr_payroll/models/__init__.py
+++ l10n_ch_hr_payroll/models/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2012 David Coninckx (Opennet Sarl)
+# © 2016 David Coninckx (Opennet Sarl), Ermin Trevisan (twanda AG)
 # License into __openerp__.py.
 
 from . import hr_contract
 from . import hr_employee
+from . import hr_salary_rule
+from . import hr_salary_rule_category
--- l10n_ch_hr_payroll/models/hr_contract.py
+++ l10n_ch_hr_payroll/models/hr_contract.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2012 David Coninckx (Opennet Sarl)
+# Make additional fields translatable by Ermin Trevisan (twanda AG)
 # License into __openerp__.py.
 
 from openerp import fields, models
@@ -9,8 +10,8 @@ import openerp.addons.decimal_precision as dp
 class HrContract(models.Model):
     _inherit = 'hr.contract'
 
-    holiday_rate = fields.Float(string='Holiday Rate')
-    lpp_rate = fields.Float(string='LPP Rate',
+    holiday_rate = fields.Float(string='Holiday Rate', translate=True)
+    obp_rate = fields.Float(string='OBP Rate', translate=True,
                             digits=dp.get_precision('Payroll Rate'))
-    lpp_amount = fields.Float(string='LPP Amount',
+    obp_amount = fields.Float(string='OBP Amount', translate=True,
                               digits=dp.get_precision('Account'))
