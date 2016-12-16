7db25212d8c18a5030f11e1c1e291fc8f34aef8f
Akim Juillerat <akim.juillerat@camptocamp.com>
2016-12-16 14:28:44 +0100

    Change import openerp to odoo

--- l10n_ch_base_bank/tests/test_bank_type.py
+++ l10n_ch_base_bank/tests/test_bank_type.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2015 Yannick Vaucher (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp.tests import common
+from odoo.tests import common
 
 
 class TestBankType(common.TransactionCase):
