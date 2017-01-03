7b8d65043c4ddf365d48dfc46ae2a02c2cb851ba
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-01-03 11:08:38 +0100

    [MIG] l10n_ch_states: Migration to 10.0

--- l10n_ch_states/__manifest__.py
+++ l10n_ch_states/__manifest__.py
@@ -1,28 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Mathias Neef
-#    Copyright 2015 copadoMEDIA UG
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015 Mathias Neef copadoMEDIA UG
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Switzerland Country States',
     'category': 'Localisation',
     'summary': '',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'author': 'copado MEDIA UG, Odoo Community Association (OCA)',
     'website': 'http://www.copado.de',
     'license': 'AGPL-3',
