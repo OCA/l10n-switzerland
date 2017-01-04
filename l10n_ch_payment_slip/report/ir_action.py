a40a2baa23d5328b476cb534ef15040e4fca6ca8
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-01-04 13:42:23 +0100

    Port to new api

--- l10n_ch_payment_slip/report/ir_action.py
+++ l10n_ch_payment_slip/report/ir_action.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2012-2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models
+from odoo import models, fields, api
 from odoo.addons.base.ir.ir_actions import IrActionsReportXml as root
 
 
@@ -9,50 +9,39 @@ class IrActionsReportXMLReportlab(models.Model):
 
     _inherit = 'ir.actions.report.xml'
 
-    def __init__(self, cr, uid):
-        """Old school hack to extend selection fields"""
-        super(IrActionsReportXMLReportlab, self).__init__(cr, uid)
-        if not any(x for x in root._columns['report_type'].selection
-                   if x[0] == 'reportlab-pdf'):
-            root._columns['report_type'].selection.append(
-                ('reportlab-pdf', 'Report renderer')
-            )
+    report_type = fields.Selection(selection_add=[('reportlab-pdf',
+                                                   'Report renderer')])
 
-    def _lookup_report(self, cr, name):
-        cr.execute("SELECT * FROM ir_act_report_xml WHERE report_name=%s",
-                   (name,))
-        report = cr.dictfetchone()
+    @api.model
+    def _lookup_report(self, name):
+        self.env.cr.execute("SELECT * FROM ir_act_report_xml WHERE report_name=%s",
+                    (name,))
+        report = self.env.cr.dictfetchone()
         if report and report['report_type'] == 'reportlab-pdf':
             return report['report_name']
         else:
             return super(IrActionsReportXMLReportlab, self)._lookup_report(
-                cr,
-                name
-            )
+                name)
 
-    def render_report(self, cr, uid, res_ids, name, data, context=None):
+    @api.model
+    def render_report(self, res_ids, name, data):
         """
         Override to work with real pdf in testing and not html
         as we use reportlab as renderer
         """
+        context = self._context
         if context is None:
             context = {}
         if context.get('force_pdf'):
-            new_report = self._lookup_report(cr, name)
+            new_report = self._lookup_report(name)
             if isinstance(new_report, (str, unicode)):
-                return self.pool['report'].get_pdf(
-                    cr,
-                    uid,
+                return self.env['report'].get_pdf(
                     res_ids,
                     new_report,
                     data=data,
-                    context=context
-                ), 'pdf'
+                    ), 'pdf'
         return super(IrActionsReportXMLReportlab, self).render_report(
-            cr,
-            uid,
             res_ids,
             name,
             data,
-            context=context
-        )
+            )
