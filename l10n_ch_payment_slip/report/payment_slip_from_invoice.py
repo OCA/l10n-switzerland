cd2fe0dd6ced0e2ac68b4343203be2209cbade77
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-01-04 13:42:23 +0100

    Port to new api

--- l10n_ch_payment_slip/report/payment_slip_from_invoice.py
+++ l10n_ch_payment_slip/report/payment_slip_from_invoice.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2014-2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models
+from odoo import models, api
 
 
 class BVRFromInvoice(models.AbstractModel):
@@ -12,27 +12,21 @@ class ExtendedReport(models.Model):
 
     _inherit = 'report'
 
-    def _generate_one_slip_per_page_from_invoice_pdf(self, cr, uid, ids,
-                                                     report_name=None,
-                                                     context=None):
+    @api.multi
+    def _generate_one_slip_per_page_from_invoice_pdf(self, report_name=None):
         """Generate payment slip PDF(s) from report model.
         If there is many pdf they are merged in memory or on
         file system based on company settings
 
         :return: the generated PDF content
         """
-        user_model = self.pool['res.users']
-        slip_model = self.pool['l10n_ch.payment_slip']
-        invoice_model = self.pool['account.invoice']
-        company = user_model.browse(cr, uid, uid, context=context).company_id
-        invoices = invoice_model.browse(cr, uid, ids, context=context)
+        user_model = self.env['res.users']
+        slip_model = self.env['l10n_ch.payment_slip']
+        invoice_model = self.env['account.invoice']
+        company = user_model.browse().company_id
+        invoices = invoice_model.browse()
 
-        docs = slip_model.compute_pay_slips_from_invoices(
-            cr,
-            uid,
-            invoices,
-            context=context
-        )
+        docs = slip_model.compute_pay_slips_from_invoices(invoices)
         if len(docs) == 1:
             return docs[0]._draw_payment_slip(a4=True,
                                               b64=False,
@@ -46,24 +40,16 @@ class ExtendedReport(models.Model):
                 return self.merge_pdf_in_memory(pdfs)
             return self.merge_pdf_on_disk(pdfs)
 
-    def get_pdf(self, cr, uid, ids, report_name, html=None, data=None,
-                context=None):
+    @api.multi
+    def get_pdf(self, report_name, html=None, data=None):
         if (report_name == 'l10n_ch_payment_slip.'
                            'one_slip_per_page_from_invoice'):
             return self._generate_one_slip_per_page_from_invoice_pdf(
-                cr,
-                uid,
-                ids,
-                context=context,
                 report_name=report_name,
             )
         else:
             return super(ExtendedReport, self).get_pdf(
-                cr,
-                uid,
-                ids,
                 report_name,
                 html=html,
-                data=data,
-                context=context
+                data=data
             )
