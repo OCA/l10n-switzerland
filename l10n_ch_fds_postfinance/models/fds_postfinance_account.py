48fa825dc7b8f4e85b38514159cd2540cc836229
Philipp Hug <philipp@hug.cx>
2017-01-21 17:12:19 +0100

    Disable host key checking

--- l10n_ch_fds_postfinance/models/fds_postfinance_account.py
+++ l10n_ch_fds_postfinance/models/fds_postfinance_account.py
@@ -114,10 +114,13 @@ class FdsPostfinanceAccount(models.Model):
             key_pass = self.authentication_key_ids.config()
 
             # connect sftp
+            cnopts = pysftp.CnOpts()
+            cnopts.hostkeys = None
             with pysftp.Connection(self.hostname,
                                    username=self.username,
                                    private_key=tmp_key.name,
-                                   private_key_pass=key_pass) as sftp:
+                                   private_key_pass=key_pass,
+                                   cnopts=cnopts) as sftp:
 
                 directories = sftp.listdir()
 
