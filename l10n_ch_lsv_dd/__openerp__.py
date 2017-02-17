92a111105fc468bd77d45738b39599611cdb116c
BT-cserra <carlos.serra@braintec-group.com>
2017-02-17 16:46:48 +0100

    [t6439] Implemented a first version for the pain.008.001.02.ch.03. May contain errors.

--- l10n_ch_lsv_dd/__openerp__.py
+++ l10n_ch_lsv_dd/__openerp__.py
@@ -2,6 +2,7 @@
 #
 #    Swiss localization Direct Debit module for Odoo
 #    Copyright (C) 2014 Compassion (http://www.compassion.ch)
+#    Copyright (C) 2017 brain-tec AG (http://www.braintec-group.com)
 #    @author: Cyril Sester <cyril.sester@outlook.com>
 #
 #    This module has been inspired by the SEPA Direct Debit module
@@ -26,14 +27,16 @@
     'summary': 'Create LSV and Direct Debit (postfinance) files',
     'version': '9.0.1.0.0',
     'license': 'AGPL-3',
-    'author': "Compassion,Odoo Community Association (OCA)",
-    'website': 'http://www.compassion.ch',
+    'author': "brain-tec AG,Compassion,Odoo Community Association (OCA)",
+    'website': 'http://www.compassion.ch,http://www.braintec-group.com',
     'category': 'Localization',
     'depends': [
         'l10n_ch_payment_slip',
         'account_payment_order',
         'account_payment_mode',
         'account_banking_mandate',
+        'account_banking_pain_base',
+        'account_banking_sepa_direct_debit',
     ],
     'external_dependencies': {},
     'data': [
@@ -41,12 +44,15 @@
         'data/export_filename_sequence.xml',
         'views/account_payment_line_view.xml',
         'views/account_payment_method_view.xml',
+        'views/account_payment_mode_view.xml',
         'views/account_payment_order_view.xml',
         'views/banking_export_ch_dd_view.xml',
+        'views/bank_payment_line_view.xml',
         'views/bank_view.xml',
         'views/dd_export_wizard_view.xml',
         'views/invoice_view.xml',
         'views/lsv_export_wizard_view.xml',
+        'views/sepa_dd_export_wizard_view.xml',
         'security/ir.model.access.csv',
     ],
     'demo': [],
