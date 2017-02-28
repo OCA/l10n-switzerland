8185543e0a469c54aecd3dc4b48889ce6ad60927
Denis Leemann <denis.leemann@camptocamp.com>
2017-02-28 18:16:52 +0100

    [MIG] l10n_ch_pain_credit_transfer: Migrated to 10.0

--- l10n_ch_pain_credit_transfer/__manifest__.py
+++ l10n_ch_pain_credit_transfer/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# copyright 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Switzerland - PAIN Credit Transfer",
     "summary": "Generate ISO 20022 credit transfert (SEPA and not SEPA)",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Finance",
     "author": "Akretion,Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
@@ -13,5 +13,5 @@
         "l10n_ch_pain_base",
         "account_banking_sepa_credit_transfer",
     ],
-    'installable': False,
+    'installable': True,
 }
