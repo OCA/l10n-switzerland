e870a3b340671b2a11372251e2ed18b5af7ce996
Denis Leemann <denis.leemann@camptocamp.com>
2017-03-01 13:20:34 +0100

    Update to OCA standards

--- l10n_ch_dta/post_install.py
+++ l10n_ch_dta/post_install.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Braintec AG (Kumar Aberer <kumar.aberer@braintec-group.com>)
+# Copyright 2016 Braintec AG (Kumar Aberer <kumar.aberer@braintec-group.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, SUPERUSER_ID
