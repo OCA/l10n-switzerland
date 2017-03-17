288fbe651e399e5af06399afe1f4104b91b0683d
Open Net Sarl <jae@open-net.ch>
2017-03-17 17:25:38 +0100

    Added More tests

--- l10n_ch_hr_payroll/tests/__init__.py
+++ l10n_ch_hr_payroll/tests/__init__.py
@@ -2,4 +2,7 @@
 # © 2017 Leonardo Franja (Open Net Sarl)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import test_all
+from . import test_contract_lpp
+from . import test_max_lpp
+from . import test_min_lpp
+from . import test_no_lpp_rate
