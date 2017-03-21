3ae52970f2867195e21663af515811e21fe38314
Open Net Sarl <jae@open-net.ch>
2017-03-21 08:54:48 +0100

    - Fixed permitions on configuration wizard and lpp contract

--- l10n_ch_hr_payroll/__manifest__.py
+++ l10n_ch_hr_payroll/__manifest__.py
@@ -46,14 +46,14 @@
     'website': 'http://open-net.ch',
     'license': 'AGPL-3',
     'data': [
+        'security/ir.model.access.csv',
         'data/hr.salary.rule.category.xml',
         'data/hr.salary.rule.xml',
         'views/hr_employee_view.xml',
         'views/hr_contract_view.xml',
         'views/hr_payroll_config_view.xml',
         'views/hr_payslip_view.xml',
-        'views/lpp_contract_view.xml',
-        'security/ir.model.access.csv'
+        'views/lpp_contract_view.xml'
     ],
     'installable': True
 }
