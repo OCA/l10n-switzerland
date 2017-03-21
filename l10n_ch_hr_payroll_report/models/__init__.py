e4193cdc94d51321f77626fc66dd877203d30a9b
jcoux <julien.coux@camptocamp.com>
2017-03-21 13:40:02 +0100

    Add new module: Switzerland - Payroll Reports

---
+++ l10n_ch_hr_payroll_report/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Julien Coux (Camptocamp)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import hr_payslip_yearly_report
+from . import hr_salary_rule
