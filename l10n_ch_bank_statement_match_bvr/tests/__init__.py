66d038f0daefe5d5526713c0a9ee8538649930ea
Louis Bettens <lbe@open-net.ch>
2017-04-07 11:55:45 +0200

    add unit tests
    Use a dummy importer to avoid depending on the behavior of specific
    import modules.  test_match_import.pydata is reused from
    https://github.com/OCA/bank-statement-import/pull/102 but both pull
    requests are independent.

--- l10n_ch_bank_statement_match_bvr/tests/__init__.py
+++ l10n_ch_bank_statement_match_bvr/tests/__init__.py
@@ -2,5 +2,4 @@
 # Copyright 2017 Open Net Sàrl
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import account_bank_statement_import
-from .tests import pydata_importer
+from . import test_match_import
