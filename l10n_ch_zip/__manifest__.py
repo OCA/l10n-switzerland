8e61f505f71fb4eecacb8aa59574bc60a533876f
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-05-10 11:37:59 +0200

    Bump version

--- l10n_ch_zip/__manifest__.py
+++ l10n_ch_zip/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Switzerland - Postal codes (ZIP) list',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'author': '''
         Camptocamp,
         brain-tec AG,
