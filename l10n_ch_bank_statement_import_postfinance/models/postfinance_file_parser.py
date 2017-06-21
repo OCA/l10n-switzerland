b677cd2ab9fd766becaf71e75e5ad74e3f516f00
Emanuel Cino <ecino@compassion.ch>
2017-06-21 10:58:42 +0200

    Adapt parser to new inheritance scheme of camt parser
    - Remove old module base_import

--- l10n_ch_bank_statement_import_postfinance/models/postfinance_file_parser.py
+++ l10n_ch_bank_statement_import_postfinance/models/postfinance_file_parser.py
@@ -16,13 +16,13 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp.addons.account_bank_statement_import_camt.camt import CamtParser
-
 import logging
 from os.path import splitext
 from tarfile import TarFile, TarError
 from cStringIO import StringIO
 from lxml import etree
+
+from openerp import models
 
 _logger = logging.getLogger(__name__)
 
@@ -36,61 +36,55 @@ except ImportError:
     wand = None
 
 
-class PFCamtParser(CamtParser):
-    """Parser for camt bank statement files of PostFinance SA."""
-
-    def parse_transaction_details(self, ns, node, transaction):
-        """Change how reference is read."""
-        # add file_ref to link image to bank statement line
-        self.add_value_from_node(
-            ns, node, './ns:Refs/ns:AcctSvcrRef', transaction, 'file_ref')
-        super(PFCamtParser, self).parse_transaction_details(
-            ns, node, transaction)
-
-
-class XMLPFParser(object):
+class XMLPFParser(models.AbstractModel):
     """
     Parser for XML Postfinance Statements (can be wrapped in a tar.gz file)
     """
-    def _parse(self, data_file):
-        """
-        Launch the parsing through The XML file. It sets the various
-        property of the class from the parse result.
-        This implementation expect one XML file to represents one statement
-        """
-        tree = self._check_postfinance(data_file)
-        self.currency_code = self._parse_currency_code(tree)
-        currency, account_number, statements = self.camt_parser.parse(
-            self.data_file)
-        if statements:
-            statements[0]['attachments'] = self._parse_attachments(
-                tree)
-        return self.currency_code, account_number, statements
+    _inherit = 'account.bank.statement.import.camt.parser'
 
-    def _check_postfinance(self, data_file):
+    def parse(self, data_file):
+        """
+        Handle Postfinance images of payment slips.
+        """
+        self._check_postfinance_attachments(data_file)
+
+        currency, account_number, statements = super(XMLPFParser, self).parse(
+            self.data_file)
+
+        if statements and self.attachments:
+            statements[0]['attachments'] = self._parse_attachments()
+
+        return currency, account_number, statements
+
+    def parse_transaction_details(self, ns, node, transaction):
+        """Add file_reference to find attached image"""
+        # add file_ref to link image to bank statement line
+        self.add_value_from_node(
+            ns, node, './ns:Refs/ns:AcctSvcrRef', transaction, 'file_ref')
+        super(XMLPFParser, self).parse_transaction_details(
+            ns, node, transaction)
+
+    def parse_statement(self, ns, node):
+        """
+        Find currency if not found with base parser
+        """
+        result = super(XMLPFParser, self).parse_statement(ns, node)
+        if not result.get('currency'):
+            currency_node = node.xpath(
+                '//ns:Bal[1]/ns:Amt/@Ccy', namespaces={'ns': ns})
+            if currency_node and len(currency_node) == 1:
+                result['currency'] = currency_node[0]
+        return result
+
+    def _check_postfinance_attachments(self, data_file):
         """
         Initialize parser and check that data_file can be parsed.
         :param data_file: file data
-        :return: True/False
         """
-        self.camt_parser = PFCamtParser()
         self.tar_source = data_file
         self.data_file = self._get_content_from_stream()
         if self.is_tar:
             self.attachments = self._get_attachments_from_stream(data_file)
-        else:
-            self.attachments = None
-        try:
-            root = etree.fromstring(
-                self.data_file, parser=etree.XMLParser(recover=True))
-        except etree.XMLSyntaxError:
-            root = None
-        if root is None:
-            raise ValueError(
-                'Not a valid xml file, or not an xml file at all.')
-        ns = root.tag[1:root.tag.index("}")]
-        self.camt_parser.check_version(ns, root)
-        return root
 
     def _get_content_from_stream(self):
         """Source file can be a raw or tar file. We try to guess the
@@ -153,35 +147,19 @@ class XMLPFParser(object):
         except TarError:
             return {}
 
-    def _parse_currency_code(self, tree):
-        """Parse file currency ISO code using xml tree
-        :param tree: lxml element tree instance
-        :type tree: :py:class:`lxml.etree.element.Element`
-
-        :return: the currency ISO code of the file eg: CHF
-        :rtype: string
-        """
-        ns = tree.tag[1:tree.tag.index("}")]  # namespace
-        currency_node = tree.xpath(
-            '//ns:Stmt/ns:Bal[1]/ns:Amt/@Ccy',
-            namespaces={'ns': ns})
-        if not currency_node:
-            return
-        if len(currency_node) != 1:
-            raise ValueError(
-                'Many currencies found for Postfinance statement')
-        return currency_node[0]
-
-    def _parse_attachments(self, tree):
+    def _parse_attachments(self):
         """Parse file statement to get wich attachment to use
-        :param tree: lxml element tree instance
-        :type tree: :py:class:`lxml.etree.element.Element`
-
         :return: a list of attachment tuple (name, content)
         :rtype: list
         """
         attachments = [('Statement File', self.tar_source.encode('base64'))]
         if self.is_tar:
+            # Extract XML tree
+            try:
+                tree = etree.fromstring(
+                    self.data_file, parser=etree.XMLParser(recover=True))
+            except etree.XMLSyntaxError:
+                return attachments
             ns = tree.tag[1:tree.tag.index("}")]    # namespace
             transaction_nodes = tree.xpath(
                 '//ns:Stmt/ns:Ntry/ns:AcctSvcrRef/text()',
