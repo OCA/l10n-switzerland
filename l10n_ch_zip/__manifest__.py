dfd55edbfda3c2f891acb7890dc724fe9b8fdaea
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2017-11-13 12:38:06 +0100

    [MIG] l10n_ch_zip: Migration to 11.0

--- l10n_ch_zip/__manifest__.py
+++ l10n_ch_zip/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Switzerland - Postal codes (ZIP) list',
-    'version': '10.0.1.0.1',
+    'version': '11.0.1.0.0',
     'author': '''
         Camptocamp,
         brain-tec AG,
