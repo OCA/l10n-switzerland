8b4be6c96c483abebe83c99b62c8b91ed73a27fe
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-11-14 15:20:23 +0100

    [MIG] l10n_ch_base_bank: Migration to 11.0
    Remove coding UTF-8 from headers
    Reset version to 11.0.1.0.0
    Update README.rst

--- l10n_ch_base_bank/__manifest__.py
+++ l10n_ch_base_bank/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-# © 2012 Nicolas Bessi (Camptocamp SA)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2012-2017 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {'name': 'Switzerland - Bank type',
  'summary': 'Types and number validation for swiss electronic pmnt. DTA, ESR',
- 'version': '10.0.1.1.1',
+ 'version': '11.0.1.0.0',
  'author': "Camptocamp,Odoo Community Association (OCA)",
  'category': 'Localization',
  'website': 'http://www.camptocamp.com',
