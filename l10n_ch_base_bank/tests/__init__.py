b732677916e675f5cf12d4dc4d20c23d09e35b7f
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-11-14 15:20:23 +0100

    [MIG] l10n_ch_base_bank: Migration to 11.0
    Remove coding UTF-8 from headers
    Reset version to 11.0.1.0.0
    Update README.rst
    Remove deprecated api.one

--- l10n_ch_base_bank/tests/__init__.py
+++ l10n_ch_base_bank/tests/__init__.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# © 2014-2015 Nicolas Bessi (Camptocamp SA)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import test_bank
 from . import test_bank_type
 from . import test_create_invoice
