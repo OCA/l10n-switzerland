c195c1780ab274dc92a308919dc898c9e84cbff8
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-11-14 16:25:26 +0100

    [MIG] l10n_ch_states: Migration to 11.0

--- l10n_ch_states/__manifest__.py
+++ l10n_ch_states/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Mathias Neef copadoMEDIA UG
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Switzerland Country States',
     'category': 'Localisation',
     'summary': '',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': 'copado MEDIA UG, Odoo Community Association (OCA)',
     'website': 'http://www.copado.de',
     'license': 'AGPL-3',
--- l10n_ch_states/models/res_country.py
+++ l10n_ch_states/models/res_country.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Mathias Neef copadoMEDIA UG
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
