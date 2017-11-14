8fe8b21f1f1bc99a40c7d28b93bc1afe28b24dfd
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-11-14 15:08:37 +0100

    [MIG] l10n_ch_bank: Migration to 11.0
    Remove coding UTF-8 from headers
    Fix Copyrights according to commits
    Remove tag application from this module
    Reset version to 11.0.1.0.0
    Remove old migration scripts

--- l10n_ch_bank/models/bank.py
+++ l10n_ch_bank/models/bank.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-# © 2011-2014 Nicolas Bessi (Camptocamp SA)
-# © 2014 Olivier Jossen brain-tec AG
-# © 2014 Guewen Baconnier (Camptocamp SA)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2014 Olivier Jossen brain-tec AG
+# Copyright 2014-2015 Guewen Baconnier (Camptocamp SA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import models, fields
 
 
