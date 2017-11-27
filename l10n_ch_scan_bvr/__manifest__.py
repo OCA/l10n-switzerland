36ca99cc989e5c05070809f66c9b2278a0a921ba
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-11-27 11:00:22 +0100

    Bump version

--- l10n_ch_scan_bvr/__manifest__.py
+++ l10n_ch_scan_bvr/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Switzerland - Scan ESR/BVR to create invoices",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "category": "Generic Modules/Others",
     "website": "http://www.camptocamp.com",
