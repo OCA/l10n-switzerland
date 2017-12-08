e2c23bffe0f8b2bee47648190d07c252ce8e7169
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2017-12-08 17:12:48 +0100

    Bump version

--- l10n_ch_dta/__openerp__.py
+++ l10n_ch_dta/__openerp__.py
@@ -21,7 +21,7 @@
 
 {'name': 'Switzerland - Bank Payment File (DTA)',
  'summary': 'Electronic payment file for Swiss bank (DTA)',
- 'version': '9.0.1.0.3',
+ 'version': '9.0.1.0.4',
  'author': "Camptocamp,Odoo Community Association (OCA)",
  'category': 'Localization',
  'website': 'http://www.camptocamp.com',
