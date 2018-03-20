fc621419616333fa87ab70fa1ef45f2f8f440566
Emanuel Cino <ecino@compassion.ch>
2018-03-20 16:18:30 +0100

    CO-1571 FIX lsv file generation

--- l10n_ch_lsv_dd/tests/__init__.py
+++ l10n_ch_lsv_dd/tests/__init__.py
@@ -1,22 +1,6 @@
-##############################################################################
-#
-#    Swiss localization Direct Debit module for OpenERP
-#    Copyright (C) 2017 brain-tec AG (http://www.braintec-group.com)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# (C) 2017 Carlos Serra-Toro <carlos.serra@braintec-group.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import test_bank
 from . import test_lsv_export_wizard
