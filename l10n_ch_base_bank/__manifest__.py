907bb38e94ff4a030144323c22f6d7adbd95add2
Maxence Groine <mgroine@fiefmanage.ch>
2018-06-20 16:31:17 +0200

    l10n_ch_base_bank: Add `ISR adherent` field to view + new group
    The new security group is created with no default users. It is up to
    other modules that need the `isr_adherent_num` field to be displayed to
    add users/link existing security groups to the new
    `group_isr_adherent_num_users` group

--- l10n_ch_base_bank/__manifest__.py
+++ l10n_ch_base_bank/__manifest__.py
@@ -3,13 +3,14 @@
 
 {'name': 'Switzerland - Bank type',
  'summary': 'Types and number validation for swiss electronic pmnt. DTA, ESR',
- 'version': '11.0.1.1.1',
+ 'version': '11.0.1.2.0',
  'author': "Camptocamp,Odoo Community Association (OCA)",
  'category': 'Localization',
  'website': 'http://www.camptocamp.com',
  'license': 'AGPL-3',
  'depends': ['account_payment_partner', 'base_iban'],
  'data': [
+     'security/security.xml',
      'views/bank.xml',
      'views/invoice.xml',
  ],
