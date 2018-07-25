c99222784191bc0948518658361a65c3f54aa32f
Mykhailo Panarin <31699470+mpanarin@users.noreply.github.com>
2018-07-25 18:54:12 +0300

    [ADD] l10n_ch_account_tags: add new module v11.0 (#432)

---
+++ l10n_ch_account_tags/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    'name': 'Switzerland Account Tags',
+    'category': 'Localisation',
+    'summary': '',
+    'version': '11.0.1.0.0',
+    'author': 'Camptocamp SA, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-switzerland',
+    'license': 'AGPL-3',
+    'depends': [
+        'l10n_ch',
+    ],
+    'data': [
+        'data/account_account_tag.xml',
+        'data/account_account_template.xml',
+    ],
+}
