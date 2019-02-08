890b2edebf052799e86b84291929ae298d183587
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2019-02-08 17:34:39 +0100

    [MIG] l10n_ch_pain_credit_transfer: Migration to odoo 12

--- l10n_ch_pain_credit_transfer/__manifest__.py
+++ l10n_ch_pain_credit_transfer/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Switzerland - PAIN Credit Transfer",
     "summary": "Generate ISO 20022 credit transfert (SEPA and not SEPA)",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Finance",
     "author": "Akretion,Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
@@ -14,5 +14,5 @@
         "l10n_ch_base_bank",
         "account_banking_sepa_credit_transfer",
     ],
-    'installable': False,
+    'installable': True,
 }
