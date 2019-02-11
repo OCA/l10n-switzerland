5c92511323b9dd6c6c62e7b839c5222adbd37e3e
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2019-02-11 15:51:53 +0100

    [MIG] l10n_ch_base_bank: Migration to odoo 12

--- l10n_ch_base_bank/tests/test_create_invoice.py
+++ l10n_ch_base_bank/tests/test_create_invoice.py
@@ -1,35 +1,73 @@
-# Copyright 2015 Yannick Vaucher (Camptocamp SA)
+# Copyright 2012-2019 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo.tests import common
 from odoo import exceptions
+from odoo.tests.common import Form
 
 
-class TestCreateInvoice(common.TransactionCase):
+class TestCreateInvoice(common.SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.company = cls.env.ref('base.main_company')
+        cls.partner = cls.env.ref('base.res_partner_12')
+        bank = cls.env['res.bank'].create({
+            'name': 'BCV',
+            'bic': 'BIC23423',
+            'clearing': '234234',
+        })
+        # define company bank account
+        cls.bank_journal = cls.env['account.journal'].create({
+            'company_id': cls.company.id,
+            'type': 'bank',
+            'code': 'BNK42',
+            'bank_id': bank.id,
+            'bank_acc_number': '01-1234-1',
+        })
+        cls.bank_acc = cls.bank_journal.bank_account_id
+        cls.payment_mode = cls.env['account.payment.mode'].create({
+            'name': 'Inbound Credit transfer CH',
+            'company_id': cls.company.id,
+            'bank_account_link': 'fixed',
+            'fixed_journal_id': cls.bank_journal.id,
+            'show_bank_account_from_journal': True,
+            'payment_method_id':
+                cls.env.ref('account.account_payment_method_manual_in').id,
+        })
+        cls.partner.customer_payment_mode_id = cls.payment_mode.id
+        fields_list = [
+            'company_id',
+            'user_id',
+            'currency_id',
+            'journal_id',
+        ]
+        cls.inv_values = cls.env['account.invoice'].default_get(fields_list)
+
+    def new_form(self):
+        inv = Form(
+            self.env['account.invoice'],
+            view='account.invoice_form'
+        )
+        inv.partner_id = self.partner
+        inv.journal_id = self.bank_journal
+        return inv
 
     def test_emit_invoice_with_isr_reference(self):
-        self.inv_values.update({
-            'partner_id': self.partner.id,
-            'type': 'out_invoice',
-            'journal_id': self.bank_journal.id,
-        })
-        invoice = self.env['account.invoice'].create(self.inv_values)
-        invoice._onchange_partner_id()
+        inv_form = self.new_form()
+        invoice = inv_form.save()
         self.assertEqual(invoice.partner_banks_to_show(), self.bank_acc)
         self.assertNotEqual(invoice.reference_type, 'isr')
-
+        inv_form.reference_type = 'isr'
         invoice.reference = '132000000000000000000000014'
-        invoice.onchange_reference()
-
+        inv_form.save()
         self.assertEqual(invoice.reference_type, 'isr')
 
     def test_emit_invoice_with_isr_reference_15_pos(self):
-        self.inv_values.update({
-            'partner_id': self.partner.id,
-            'type': 'out_invoice',
-            'journal_id': self.bank_journal.id,
-        })
-        invoice = self.env['account.invoice'].create(self.inv_values)
-        invoice._onchange_partner_id()
+        inv_form = self.new_form()
+        invoice = inv_form.save()
+
         self.assertEqual(invoice.partner_banks_to_show(), self.bank_acc)
         self.assertNotEqual(invoice.reference_type, 'isr')
 
@@ -38,99 +76,40 @@ class TestCreateInvoice(common.TransactionCase):
         invoice.write({'reference_type': 'isr'})
 
         # and save
-        vals = invoice._convert_to_write(invoice._cache)
-        self.env['account.invoice'].create(vals)
+        inv_form.save()
 
     def test_emit_invoice_with_non_isr_reference(self):
-        self.inv_values.update({
-            'partner_id': self.partner.id,
-            'type': 'out_invoice',
-            'journal_id': self.bank_journal.id,
-        })
-        invoice = self.env['account.invoice'].create(self.inv_values)
-        invoice._onchange_partner_id()
+        inv_form = self.new_form()
+        invoice = inv_form.save()
+
         self.assertEqual(invoice.partner_banks_to_show(), self.bank_acc)
         self.assertNotEqual(invoice.reference_type, 'isr')
 
         invoice.reference = 'Not a ISR ref with 27 chars'
-
-        invoice.onchange_reference()
-
         self.assertNotEqual(invoice.reference_type, 'isr')
 
     def test_emit_invoice_with_missing_isr_reference(self):
-        self.inv_values.update({
-            'partner_id': self.partner.id,
-            'type': 'out_invoice',
-            'account_id': 1,  # set dummy account to be replaced by onchange
-            'journal_id': self.bank_journal.id,
-        })
-        invoice = self.env['account.invoice'].create(self.inv_values)
+        inv_form = self.new_form()
+        # set dummy account to be replaced by onchange
+        inv_form.account_id = self.env['account.account'].browse(1)
+        inv_form.save()
 
         with self.assertRaises(exceptions.ValidationError):
-            invoice._onchange_partner_id()
-
-            invoice.reference = False
-            invoice.reference_type = 'isr'  # set manually ISR reference type
-
+            inv_form.reference = False
+            inv_form.reference_type = 'isr'  # set manually ISR reference type
             # and save
-            self.env['account.invoice'].create(
-                invoice._convert_to_write(invoice._cache)
-            )
+            inv_form.save()
 
     def test_emit_invoice_with_isr_reference_missing_ccp(self):
-        self.inv_values.update({
-            'partner_id': self.partner.id,
-            'type': 'out_invoice',
-            'account_id': 1,  # set dummy account to be replaced by onchange
-            'journal_id': self.bank_journal.id,
-        })
-        invoice = self.env['account.invoice'].create(self.inv_values)
+        inv_form = self.new_form()
+        inv_form.type = 'out_invoice'
+        # set dummy account to be replaced by onchange
+        inv_form.account_id = self.env['account.account'].browse(1)
+        inv_form.save()
+
         self.bank_acc.acc_number = 'not a CCP'
 
         with self.assertRaises(exceptions.ValidationError):
-            invoice._onchange_partner_id()
-
-            invoice.reference = '132000000000000000000000014'
-
-            invoice.onchange_reference()
-            invoice.reference_type = 'isr'
-            self.env['account.invoice'].create(
-                invoice._convert_to_write(invoice._cache)
-            )
-
-    def setUp(self):
-        super(TestCreateInvoice, self).setUp()
-        self.company = self.env.ref('base.main_company')
-        self.partner = self.env.ref('base.res_partner_12')
-        bank = self.env['res.bank'].create({
-            'name': 'BCV',
-            'bic': 'BIC23423',
-            'clearing': '234234',
-        })
-        # define company bank account
-        self.bank_journal = self.env['account.journal'].create({
-            'company_id': self.company.id,
-            'type': 'bank',
-            'code': 'BNK42',
-            'bank_id': bank.id,
-            'bank_acc_number': '01-1234-1',
-        })
-        self.bank_acc = self.bank_journal.bank_account_id
-        self.payment_mode = self.env['account.payment.mode'].create({
-            'name': 'Inbound Credit transfer CH',
-            'company_id': self.company.id,
-            'bank_account_link': 'fixed',
-            'fixed_journal_id': self.bank_journal.id,
-            'show_bank_account_from_journal': True,
-            'payment_method_id':
-            self.env.ref('account.account_payment_method_manual_in').id,
-        })
-        self.partner.customer_payment_mode_id = self.payment_mode.id
-        fields_list = [
-            'company_id',
-            'user_id',
-            'currency_id',
-            'journal_id',
-        ]
-        self.inv_values = self.env['account.invoice'].default_get(fields_list)
+            inv_form.reference = '132000000000000000000000014'
+            inv_form.reference_type = 'isr'
+            inv_form.save()
