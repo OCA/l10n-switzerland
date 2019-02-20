133d4387912de8adb97c684df786ded588b6c9aa
Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
2019-02-20 19:35:01 +0200

    [MIG][12.0] improvements to v12

--- l10n_ch_payment_slip/__manifest__.py
+++ l10n_ch_payment_slip/__manifest__.py
@@ -2,16 +2,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {'name': 'Switzerland - ISR inpayment slip (PVR/BVR/ESR)',
  'summary': 'Print inpayment slip from your invoices',
- 'version': '11.0.1.0.0',
+ 'version': '12.0.1.0.0',
  'author': "Camptocamp,Odoo Community Association (OCA)",
  'category': 'Localization',
  'website': 'http://www.camptocamp.com',
  'license': 'AGPL-3',
  'depends': [
      'account',
-     'account_invoicing',
      'l10n_ch_base_bank',
      'base_transaction_id',  # OCA/account-reconcile
+     'account_payment_order',
      'web',
      'l10n_ch',
  ],
