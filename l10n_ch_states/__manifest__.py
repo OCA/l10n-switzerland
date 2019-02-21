8468be7d1d74532e7bc7736e0270e07bf768344d
mpanarin <m.panarin@mobilunity.com>
2019-02-21 12:18:11 +0200

    [MIG] l10n_ch_states to v12

--- l10n_ch_states/__manifest__.py
+++ l10n_ch_states/__manifest__.py
@@ -1,18 +1,19 @@
+# Copyright 2019 Camptocamp SA
 # Copyright 2015 Mathias Neef copadoMEDIA UG
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Switzerland Country States',
     'category': 'Localisation',
     'summary': '',
-    'version': '11.0.1.0.0',
-    'author': 'copado MEDIA UG, Odoo Community Association (OCA)',
-    'website': 'http://www.copado.de',
+    'version': '12.0.1.0.0',
+    'author': 'copado MEDIA UG,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-switzerland',
     'license': 'AGPL-3',
     'depends': [
         'base',
     ],
-    'data': ['data/res_country_states.xml'],
-    'demo': [],
-    'installable': False,
-    'application': False,
+    'data': [
+        'data/res_country_states.xml'
+    ],
 }
