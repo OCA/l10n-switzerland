29648d5210d3c87838f05b0fec18630a88dbce88
Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
2019-03-20 15:24:45 +0200

    [IMP] use common method from bank-payment for bank searching

--- l10n_ch_base_bank/models/invoice.py
+++ l10n_ch_base_bank/models/invoice.py
@@ -128,12 +128,9 @@ class AccountInvoice(models.Model):
         if not self.partner_bank_id or not vals.get('partner_bank_id'):
             type_defined = vals.get('type') or self.type
             if type_defined == 'out_invoice':
-                partner = self.env.user.company_id.partner_id
-                journal = vals.get('journal_id') or self.journal_id.id
-                ref_type = vals.get('reference_type') or self.reference_type
-                vals['partner_bank_id'] = self._get_bank_id(
-                    partner, journal, ref_type,
-                )
+                banks = self.partner_banks_to_show()
+                if banks:
+                    vals['partner_bank_id'] = banks[0].id
         return super().write(vals)
 
     @api.model
