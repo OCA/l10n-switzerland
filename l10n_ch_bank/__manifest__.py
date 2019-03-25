f9b8f7058a9d4197e07548130fb47e55210cf654
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2019-03-25 14:12:39 +0100

    [MIG] l10n_ch_bank: Migration to 12.0

--- l10n_ch_bank/__manifest__.py
+++ l10n_ch_bank/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Switzerland - Bank list',
-    'version': '11.0.1.0.2',
+    'version': '12.0.1.0.0',
     'author': "Camptocamp, brain-tec AG,Odoo Community Association (OCA)",
     'category': 'Localisation',
     'website': 'http://www.camptocamp.com',
@@ -19,5 +19,5 @@
     'images': [],
     'demo': [],
     'auto_install': False,
-    'installable': False,
+    'installable': True,
 }
