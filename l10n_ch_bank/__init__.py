cca968fe3c45f1c960cc8eb511643266dcfde6c8
Iryna Vyshnevska <i.vyshnevska@mobilunity.com>
2019-03-26 14:59:30 +0200

    [IMP] cleanup pylint

--- l10n_ch_bank/__init__.py
+++ l10n_ch_bank/__init__.py
@@ -1,12 +1,4 @@
 # Copyright 2015 Yannick Vaucher (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import models
-
-
-def post_init(cr, registry):
-    """Import CSV data as it is faster than xml and because we can't use
-    noupdate anymore with csv"""
-    from openerp.tools import convert_file
-    filename = 'data/res.bank.csv'
-    convert_file(cr, 'l10n_ch_bank', filename, None, mode='init',
-                 noupdate=True, kind='init', report=None)
+from .hooks import post_init
