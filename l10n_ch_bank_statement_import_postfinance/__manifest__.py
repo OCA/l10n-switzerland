4f35151e60830520e14e564f19700b61add28c73
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2019-05-22 11:02:54 +0200

    Bump version

--- l10n_ch_bank_statement_import_postfinance/__manifest__.py
+++ l10n_ch_bank_statement_import_postfinance/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {'name': "Swiss bank statements import",
- 'version': '10.0.1.0.2',
+ 'version': '10.0.1.0.3',
  'author': "Compassion CH, Camptocamp, Odoo Community Association (OCA)",
  'category': 'Finance',
  'complexity': 'normal',
