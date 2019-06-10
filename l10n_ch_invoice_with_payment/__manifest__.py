840c3d9eb672374cb8fc462bfeb0374e6a971440
Damien Crier <damien.crier@camptocamp.com>
2019-06-10 12:01:03 +0200

    [ADD] module l10n_ch_invoice_with_payment

---
+++ l10n_ch_invoice_with_payment/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Invoice report with payment',
+    'version': '11.0.1.0.0',
+    'author': 'Camptocamp, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'category': 'Accounting',
+    'website': 'http://www.camptocamp.com',
+    'images': [],
+    'depends': [
+        'account',
+        'l10n_ch_payment_slip',
+        ],
+    'data': [
+        'data/report.xml',
+        ],
+    'installable': True,
+    'auto_install': False,
+}
