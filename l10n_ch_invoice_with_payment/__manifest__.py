93c8cac918bbb93d39a9c8f36a17b6c1474baeb6
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2019-08-20 00:06:51 +0300

    [MIG][12.0] l10n_ch_invoice_with_payment: migrate

--- l10n_ch_invoice_with_payment/__manifest__.py
+++ l10n_ch_invoice_with_payment/__manifest__.py
@@ -2,12 +2,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Invoice report with payment',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Camptocamp, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'category': 'Accounting',
     'website': 'http://www.camptocamp.com',
-    'images': [],
     'depends': [
         'account',
         'l10n_ch_payment_slip',
