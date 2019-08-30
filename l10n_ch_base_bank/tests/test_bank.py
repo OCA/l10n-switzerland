43b85e5169f94571076c80d59198c2772a320708
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2019-08-30 17:38:24 +0200

    Use Odoo core field l10n_ch_postal to replace ccp
    Plus stop trying to get bank from postal number thus remove
    the ccp fields on res.bank
    
    This will prepare the migration to version 13.0

--- l10n_ch_base_bank/tests/test_bank.py
+++ l10n_ch_base_bank/tests/test_bank.py
@@ -1,5 +1,5 @@
 # Copyright 2014-2015 Nicolas Bessi (Azure Interior SA)
-# Copyright 2015-2017 Yannick Vaucher (Camptocamp SA)
+# Copyright 2015-2019 Yannick Vaucher (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo.tests import common
 from odoo.tools import mute_logger
@@ -7,9 +7,12 @@ from odoo import exceptions
 from odoo.tests.common import Form
 from odoo.tests import tagged
 
-ch_iban = 'CH15 3881 5158 3845 3843 7'
-ch_post_iban = 'CH09 0900 0000 1000 8060 7'
-fr_iban = 'FR83 8723 4133 8709 9079 4002 530'
+CH_SUBSCRIPTION = '01-162-8'  # partner ISR subsr num we register under postal
+CH_SUBSCRIPTION_9DIGITS = '010001628'  # same ISR subsr num in 9 digits format
+CH_POSTAL = '10-8060-7'
+CH_IBAN = 'CH15 3881 5158 3845 3843 7'
+CH_POSTFINANCE_IBAN = 'CH09 0900 0000 1000 8060 7'
+FR_IBAN = 'FR83 8723 4133 8709 9079 4002 530'
 
 
 @tagged('post_install', '-at_install')
@@ -24,7 +27,6 @@ class TestBank(common.SavepointCase):
             'name': 'Alternative Bank Schweiz AG',
             'bic': 'ALSWCH21XXX',
             'clearing': '38815',
-            'ccp': '46-110-7',
         })
         cls.post_bank = cls.env['res.bank'].search(
             [('bic', '=', 'POFICHBEXXX')])
@@ -32,14 +34,13 @@ class TestBank(common.SavepointCase):
             cls.post_bank = cls.env['res.bank'].create({
                 'name': 'PostFinance AG',
                 'bic': 'POFICHBEXXX',
-                'clearing': '9000',
-                'ccp': '10-8060-7',
+                'clearing': '09000',
             })
 
     def new_form(self):
         form = Form(
             self.env['res.partner.bank'],
-            view='l10n_ch_base_bank.add_ccp_on_res_partner_bank'
+            view='l10n_ch_base_bank.add_postal_on_res_partner_bank'
         )
         form.partner_id = self.partner
         return form
@@ -48,104 +49,145 @@ class TestBank(common.SavepointCase):
         # in some cases we need form without partner
         form = Form(
             self.env['res.partner.bank'],
-            view='l10n_ch_base_bank.add_ccp_on_res_partner_bank'
+            view='l10n_ch_base_bank.add_postal_on_res_partner_bank'
         )
         return form
 
     def test_bank_iban(self):
 
         bank_acc = self.new_form()
-        bank_acc.acc_number = ch_iban.replace(' ', '')
+        bank_acc.acc_number = CH_IBAN.replace(' ', '')
         account = bank_acc.save()
 
         self.assertEqual(account.bank_id, self.bank)
-        self.assertEqual(account.acc_number, ch_iban)
-        self.assertEqual(account.ccp, '46-110-7')
+        self.assertEqual(account.acc_number, CH_IBAN)
+        self.assertFalse(account.l10n_ch_postal)
         self.assertEqual(account.acc_type, 'iban')
 
     def test_bank_iban_with_spaces(self):
         bank_acc = self.new_form()
-        bank_acc.acc_number = ch_iban
+        bank_acc.acc_number = CH_IBAN
         account = bank_acc.save()
 
         self.assertEqual(account.bank_id, self.bank)
-        self.assertEqual(account.acc_number, ch_iban)
-        self.assertEqual(account.ccp, '46-110-7')
+        self.assertEqual(account.acc_number, CH_IBAN)
+        self.assertFalse(account.l10n_ch_postal)
         self.assertEqual(account.acc_type, 'iban')
 
     def test_bank_iban_lower_case(self):
         bank_acc = self.new_form()
-        bank_acc.acc_number = ch_iban.lower()
+        bank_acc.acc_number = CH_IBAN.lower()
         account = bank_acc.save()
 
         self.assertEqual(account.bank_id, self.bank)
-        self.assertEqual(account.acc_number, ch_iban.lower())
-        self.assertEqual(account.ccp, '46-110-7')
+        self.assertEqual(account.acc_number, CH_IBAN.lower())
+        self.assertFalse(account.l10n_ch_postal)
         self.assertEqual(account.acc_type, 'iban')
 
     def test_bank_iban_foreign(self):
         bank_acc = self.new_form()
-        bank_acc.acc_number = fr_iban
+        bank_acc.acc_number = FR_IBAN
         account = bank_acc.save()
 
         self.assertFalse(account.bank_id)
-        self.assertEqual(account.acc_number, fr_iban)
-        self.assertFalse(account.ccp)
+        self.assertEqual(account.acc_number, FR_IBAN)
+        self.assertFalse(account.l10n_ch_postal)
         self.assertEqual(account.acc_type, 'iban')
 
-    def test_bank_ccp(self):
+    def test_bank_postal(self):
         # test UI onchange methods
         bank_acc = self.new_form()
-        bank_acc.acc_number = '46-110-7'
+        bank_acc.acc_number = CH_SUBSCRIPTION
+        bank_acc.bank_id = self.bank
         account = bank_acc.save()
 
         self.assertEqual(account.bank_id, self.bank)
-        self.assertEqual(account.acc_number, 'Azure Interior/CCP 46-110-7')
-        self.assertEqual(account.ccp, '46-110-7')
+        self.assertEqual(
+            account.acc_number,
+            'Azure Interior/Postal number {}'.format(CH_SUBSCRIPTION)
+        )
+        self.assertEqual(account.l10n_ch_postal, CH_SUBSCRIPTION)
         self.assertEqual(account.acc_type, 'bank')
 
-    def test_bank_ccp_no_found(self):
+    def test_postal_set_bank_post(self):
         bank_acc = self.new_form()
-        bank_acc.acc_number = '10-8060-7'
+        bank_acc.acc_number = CH_POSTAL
         account = bank_acc.save()
 
-        self.assertEqual(account.bank_id, self.post_bank)
+        self.assertFalse(account.bank_id)
         # if acc_number given by user don't update it
-        self.assertEqual(account.acc_number, '10-8060-7')
-        self.assertEqual(account.ccp, '10-8060-7')
-        self.assertEqual(account.acc_type, 'postal')
+        self.assertEqual(
+            account.acc_number,
+            'Azure Interior/Postal number {}'.format(CH_POSTAL)
+        )
+        self.assertEqual(account.l10n_ch_postal, CH_POSTAL)
+        self.assertEqual(account.acc_type, 'bank')
 
         bank_acc.bank_id = self.post_bank
         bank_acc.save()
 
         self.assertEqual(account.bank_id, self.post_bank)
-        # copied ccp to acc_number
-        self.assertEqual(account.acc_number, '10-8060-7')
-        self.assertEqual(account.ccp, '10-8060-7')
+        self.assertEqual(account.acc_number, CH_POSTAL)
+        self.assertEqual(account.l10n_ch_postal, CH_POSTAL)
         self.assertEqual(account.acc_type, 'postal')
 
-    def test_ccp(self):
+    def test_postal_with_bank(self):
         bank_acc = self.new_form()
-        bank_acc.acc_number = '10-8060-7'
+        bank_acc.acc_number = CH_POSTAL
+        bank_acc.bank_id = self.post_bank
         account = bank_acc.save()
 
         self.assertEqual(account.bank_id, self.post_bank)
-        self.assertEqual(account.acc_number, '10-8060-7')
-        self.assertEqual(account.ccp, '10-8060-7')
+        self.assertEqual(account.acc_number, CH_POSTAL)
+        self.assertEqual(account.l10n_ch_postal, CH_POSTAL)
         self.assertEqual(account.acc_type, 'postal')
 
-    def test_iban_ccp(self):
+    def test_postal_without_bank(self):
+        # We don't know it is a postal account
         bank_acc = self.new_form()
-        bank_acc.acc_number = ch_post_iban
+        bank_acc.acc_number = CH_POSTAL
+        account = bank_acc.save()
+
+        self.assertFalse(account.bank_id)
+        self.assertEqual(
+            account.acc_number,
+            'Azure Interior/Postal number {}'.format(CH_POSTAL)
+        )
+        self.assertEqual(account.l10n_ch_postal, CH_POSTAL)
+        self.assertEqual(account.acc_type, 'bank')
+
+    def test_iban_postal(self):
+        bank_acc = self.new_form()
+        bank_acc.acc_number = CH_POSTFINANCE_IBAN.replace(' ', '')
         account = bank_acc.save()
 
         self.assertEqual(account.bank_id, self.post_bank)
-        self.assertEqual(account.acc_number, ch_post_iban)
-        self.assertEqual(account.ccp, '10-8060-7')
+        self.assertEqual(account.acc_number, CH_POSTFINANCE_IBAN)
+        self.assertEqual(account.l10n_ch_postal, CH_POSTAL)
+        self.assertEqual(account.acc_type, 'iban')
+
+    def test_iban_postal_with_spaces(self):
+        bank_acc = self.new_form()
+        bank_acc.acc_number = CH_POSTFINANCE_IBAN
+        account = bank_acc.save()
+
+        self.assertEqual(account.bank_id, self.post_bank)
+        self.assertEqual(account.acc_number, CH_POSTFINANCE_IBAN)
+        self.assertEqual(account.l10n_ch_postal, CH_POSTAL)
+        self.assertEqual(account.acc_type, 'iban')
+
+    def test_iban_postal_lower_case(self):
+        bank_acc = self.new_form()
+        bank_acc.acc_number = CH_POSTFINANCE_IBAN.lower()
+        account = bank_acc.save()
+
+        self.assertEqual(account.bank_id, self.post_bank)
+        self.assertEqual(account.acc_number, CH_POSTFINANCE_IBAN.lower())
+        self.assertEqual(account.l10n_ch_postal, CH_POSTAL)
         self.assertEqual(account.acc_type, 'iban')
 
     def test_other_bank(self):
-        self.bank.ccp = False
+        self.bank.l10n_ch_postal = False
         bank_acc = self.new_form()
         # the sequence is important
         bank_acc.bank_id = self.bank
@@ -154,24 +196,15 @@ class TestBank(common.SavepointCase):
 
         self.assertEqual(account.bank_id, self.bank)
         self.assertEqual(account.acc_number, 'R 12312123')
-        self.assertEqual(account.ccp, False)
+        self.assertEqual(account.l10n_ch_postal, False)
         self.assertEqual(account.acc_type, 'bank')
 
-    def test_set_ccp(self):
-        bank_acc = self.new_form()
-        bank_acc.acc_number = None
-        bank_acc.acc_number = '10-8060-7'
-        account = bank_acc.save()
-
-        self.assertEqual(account.acc_number, '10-8060-7')
-        self.assertEqual(account.bank_id, self.post_bank)
-
-    def test_set_ccp_bank(self):
+    def test_set_postal_bank(self):
         # we create bank account
         # action runs in UI before creation
         bank_acc = self.new_form()
         bank_acc.acc_number = None
-        bank_acc.ccp = '10-8060-7'
+        bank_acc.l10n_ch_postal = CH_POSTAL
         bank_acc.bank_id = self.bank
         account = bank_acc.save()
 
@@ -179,78 +212,83 @@ class TestBank(common.SavepointCase):
         # this he has ccp, new acc_number
 
         self.assertEqual(account.acc_number,
-                         'Azure Interior/CCP 10-8060-7')
-        self.assertEqual(account.ccp, '10-8060-7')
+                         'Azure Interior/Postal number {}'.format(CH_POSTAL))
+        self.assertEqual(account.l10n_ch_postal, CH_POSTAL)
         self.assertEqual(account.bank_id, self.bank)
 
-    def test_set_onchange_ccp_post(self):
-        # fond bank based on ccp set acc_number
-        # action runs in UI before creation
-        bank_acc = self.new_form()
-        bank_acc.ccp = '10-8060-7'
-        account = bank_acc.save()
-
-        self.assertEqual(account.acc_number, '10-8060-7')
-        self.assertEqual(account.ccp, '10-8060-7')
-        self.assertEqual(account.bank_id, self.post_bank)
-
-    def test_constraint_ccp(self):
+    def test_constraint_postal(self):
         with self.assertRaises(exceptions.ValidationError):
             with mute_logger():
                 self.env['res.partner.bank'].create({
                     'partner_id': self.partner.id,
                     'bank_id': self.bank.id,
                     'acc_number': 'R 12312123',
-                    'ccp': '520-54025-54054',
+                    'l10n_ch_postal': '520-54025-54054',
                 })
 
-    def test_constraint_ccp_at_bank(self):
-        with self.assertRaises(exceptions.ValidationError):
-            with mute_logger():
-                self.bank.ccp = '999999999999'
-
-    def test_constraint_adherent_number(self):
+    def test_constraint_subscription_number(self):
         with self.assertRaises(exceptions.ValidationError):
             with mute_logger():
                 self.env['res.partner.bank'].create({
                     'partner_id': self.partner.id,
                     'acc_number': '12312123',
-                    'isr_adherent_num': 'Wrong ISR adherent number',
+                    'l10n_ch_isr_subscription_chf': 'Not a valid number',
                 })
+
+        self.env['res.partner.bank'].create({
+            'partner_id': self.partner.id,
+            'acc_number': '12312123',
+            'l10n_ch_isr_subscription_chf': CH_SUBSCRIPTION,
+        })
+
+        self.env['res.partner.bank'].create({
+            'partner_id': self.partner.id,
+            'acc_number': '12312124',
+            'l10n_ch_isr_subscription_chf': CH_SUBSCRIPTION_9DIGITS,
+        })
 
     def test_create_bank_default_acc_number(self):
         bank_acc = self.new_form()
         bank_acc.bank_id = self.bank
-        bank_acc.ccp = '46-110-7'
+        bank_acc.l10n_ch_postal = CH_SUBSCRIPTION
         account = bank_acc.save()
 
         # account number set based on ccp
-        self.assertEqual(account.acc_number, 'Azure Interior/CCP 46-110-7')
-        self.assertEqual(account.ccp, '46-110-7')
+        self.assertEqual(
+            account.acc_number,
+            'Azure Interior/Postal number {}'.format(CH_SUBSCRIPTION)
+        )
+        self.assertEqual(account.l10n_ch_postal, CH_SUBSCRIPTION)
 
     def test_onchange_post_bank_acc_number(self):
         bank_acc = self.new_empty_form()
         bank_acc.bank_id = self.post_bank
-        bank_acc.ccp = '10-8060-7'
+        bank_acc.l10n_ch_postal = CH_POSTAL
 
-        self.assertEqual(bank_acc.ccp, '10-8060-7')
-        self.assertEqual(bank_acc.acc_number, '10-8060-7')
+        self.assertEqual(bank_acc.l10n_ch_postal, CH_POSTAL)
+        self.assertEqual(bank_acc.acc_number, CH_POSTAL)
 
         # if it's postal update acc_number after the cpp number
-        bank_acc.ccp = '46-110-7'
-        self.assertEqual(bank_acc.acc_number, 'CCP 46-110-7')
+        bank_acc.l10n_ch_postal = CH_SUBSCRIPTION
+        self.assertEqual(
+            bank_acc.acc_number,
+            'Postal number {}'.format(CH_SUBSCRIPTION)
+        )
 
         bank_acc.partner_id = self.partner
-        self.assertEqual(bank_acc.acc_number, 'Azure Interior/CCP 46-110-7')
-        self.assertEqual(bank_acc.ccp, '46-110-7')
+        self.assertEqual(
+            bank_acc.acc_number,
+            'Azure Interior/Postal number {}'.format(CH_SUBSCRIPTION)
+        )
+        self.assertEqual(bank_acc.l10n_ch_postal, CH_SUBSCRIPTION)
 
     def test_onchange_post_bank_ccp_in_acc_number(self):
         bank_acc = self.new_form()
-        bank_acc.acc_number = '46-110-7'
+        bank_acc.acc_number = CH_SUBSCRIPTION
         bank_acc.bank_id = self.post_bank
 
-        self.assertEqual(bank_acc.ccp, '46-110-7')
-        self.assertEqual(bank_acc.acc_number, '46-110-7')
+        self.assertEqual(bank_acc.l10n_ch_postal, CH_SUBSCRIPTION)
+        self.assertEqual(bank_acc.acc_number, CH_SUBSCRIPTION)
 
     def test_name_search(self):
         self.bank.bic = 'BBAVBEBB'
@@ -266,39 +304,45 @@ class TestBank(common.SavepointCase):
         result = self.env['res.bank'].name_search('Lausanne-Centre')
         self.assertEqual(result and result[0][0], self.bank.id)
 
-    def test_multiple_ccp_number_for_same_partner(self):
+    def test_multiple_postal_number_for_same_partner(self):
         bank_acc = self.new_form()
-        bank_acc.acc_number = '46-110-7'
+        bank_acc.acc_number = CH_SUBSCRIPTION
         bank_acc.save()
         bank_acc_2 = self.new_form()
-        bank_acc_2.acc_number = '46-110-7'
+        bank_acc_2.acc_number = CH_SUBSCRIPTION
         account2 = bank_acc_2.save()
 
-        self.assertEqual(account2.bank_id, self.bank)
-        self.assertEqual(account2.acc_number,
-                         'Azure Interior/CCP 46-110-7 #1')
+        self.assertFalse(account2.bank_id)
+        self.assertEqual(
+            account2.acc_number,
+            'Azure Interior/Postal number {} #1'.format(CH_SUBSCRIPTION)
+        )
         self.assertEqual(account2.acc_type, 'bank')
 
         bank_acc_3 = self.new_form()
-        bank_acc_3.acc_number = '46-110-7'
+        bank_acc_3.acc_number = CH_SUBSCRIPTION
         account3 = bank_acc_3.save()
 
         # no bank matches
-        self.assertEqual(account3.bank_id, self.bank)
-        self.assertEqual(account3.acc_number,
-                         'Azure Interior/CCP 46-110-7 #2')
+        self.assertFalse(account3.bank_id)
+        self.assertEqual(
+            account3.acc_number,
+            'Azure Interior/Postal number {} #2'.format(CH_SUBSCRIPTION)
+        )
         self.assertEqual(account3.acc_type, 'bank')
         account3.unlink()
         # next acc_numbers properly generated
 
         bank_acc_4 = self.new_form()
-        bank_acc_4.acc_number = '46-110-7'
+        bank_acc_4.acc_number = CH_SUBSCRIPTION
         account4 = bank_acc_4.save()
 
         account4.onchange_acc_number_set_swiss_bank()
         account4.onchange_bank_set_acc_number()
-        self.assertEqual(account4.acc_number,
-                         'Azure Interior/CCP 46-110-7 #3')
+        self.assertEqual(
+            account4.acc_number,
+            'Azure Interior/Postal number {} #2'.format(CH_SUBSCRIPTION)
+        )
 
     def test_acc_name_generation(self):
         # this test runs directly with object and onchange methods as Form
@@ -308,11 +352,11 @@ class TestBank(common.SavepointCase):
 
         # we test only proper name generation in different conditions
         account = self.env['res.partner.bank'].new({
-            'acc_number': '46-110-7',
+            'acc_number': CH_SUBSCRIPTION,
             'partner_id': False,
         })
         # acc_number is ok in first
-        self.assertEqual(account.acc_number, '46-110-7')
+        self.assertEqual(account.acc_number, CH_SUBSCRIPTION)
         # but if some onchange trigger recompilation of name we flash any name
         # only it's not iban type
         account._update_acc_name()
@@ -321,18 +365,25 @@ class TestBank(common.SavepointCase):
         account.partner_id = self.partner
         account._update_acc_name()
         self.assertEqual(account.acc_number, '')
-        account.ccp = '46-110-7'
+        account.l10n_ch_postal = CH_SUBSCRIPTION
         account._update_acc_name()
-        self.assertEqual(account.acc_number, 'Azure Interior/CCP 46-110-7')
+        self.assertEqual(
+            account.acc_number,
+            'Azure Interior/Postal number {}'.format(CH_SUBSCRIPTION)
+        )
         # remove partner name
         account.partner_id = ''
         account._update_acc_name()
-        self.assertEqual(account.acc_number, 'CCP 46-110-7')
+        self.assertEqual(
+            account.acc_number, 'Postal number {}'.format(CH_SUBSCRIPTION)
+        )
         # no changes for bank changes
         account.bank_id = self.bank
         account._update_acc_name()
-        self.assertEqual(account.acc_number, 'CCP 46-110-7')
+        self.assertEqual(
+            account.acc_number, 'Postal number {}'.format(CH_SUBSCRIPTION)
+        )
         # everything cleanup
-        account.ccp = ''
+        account.l10n_ch_postal = ''
         account._update_acc_name()
         self.assertEqual(account.acc_number, '')
