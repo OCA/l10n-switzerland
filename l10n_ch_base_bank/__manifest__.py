c031625df6f890d628664eb552001afd5c6c6f81
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2019-08-30 17:38:24 +0200

    Use Odoo core field l10n_ch_postal to replace ccp
    Plus stop trying to get bank from postal number thus remove
    the ccp fields on res.bank
    
    This will prepare the migration to version 13.0

--- l10n_ch_base_bank/__manifest__.py
+++ l10n_ch_base_bank/__manifest__.py
@@ -3,15 +3,15 @@
 
 {'name': 'Switzerland - Bank type',
  'summary': 'Types and number validation for swiss electronic pmnt. DTA, ESR',
- 'version': '12.0.1.0.0',
+ 'version': '12.0.2.0.0',
  'author': "Camptocamp,Odoo Community Association (OCA)",
  'category': 'Localization',
  'website': 'https://github.com/OCA/l10n-switzerland',
  'license': 'AGPL-3',
  'depends': [
-     'account_payment_order',
-     'account_payment_partner',
-     'base_iban'],
+     'base_iban',
+     'l10n_ch'
+ ],
  'data': [
      'views/bank.xml',
      'views/invoice.xml',
