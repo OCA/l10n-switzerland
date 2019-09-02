59b9ae88e1a27a5c4fe8ea7eb00efa4545ca543a
Timon Tschanz <timon.tschanz@gmail.com>
2019-09-02 16:12:54 +0200

    [10.0][ADD] l10n_ch_account_reconcile_isr (#492)
    [10.0][ADD] l10n_ch_account_reconcile_isr

---
+++ l10n_ch_account_reconcile_isr/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Switzerland - ISR account reconcile',
+    'version': '10.0.1.0.0',
+    'author': "Camptocamp,Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/l10n-switzerland',
+    'license': 'AGPL-3',
+    'summary': 'Adds a second automatic reconciliation button,'
+    ' which is based on the isr',
+    'depends': [
+        'account',
+        'base_transaction_id',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'qweb': [
+        'static/src/xml/account_reconciliation.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
