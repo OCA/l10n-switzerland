a8e26b8d347adf9caba698730c8e00492e14abf0
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2019-09-30 16:26:12 +0300

    [13.0][MIG] l10n_ch_account_tags: migrate v13

--- l10n_ch_account_tags/__manifest__.py
+++ l10n_ch_account_tags/__manifest__.py
@@ -1,19 +1,14 @@
-# Copyright 2018 Camptocamp SA
+# Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Switzerland Account Tags',
-    'category': 'Localisation',
-    'summary': '',
-    'version': '12.0.1.0.0',
-    'author': 'Camptocamp SA, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-switzerland',
-    'license': 'AGPL-3',
-    'depends': [
-        'l10n_ch',
-    ],
-    'data': [
-        'data/account.account.tag.csv',
-        'data/account.account.template.csv',
-    ],
+    "name": "Switzerland Account Tags",
+    "category": "Localisation",
+    "summary": "",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-switzerland",
+    "license": "AGPL-3",
+    "depends": ["l10n_ch"],
+    "data": ["data/account.account.tag.csv", "data/account.account.template.csv"],
 }
