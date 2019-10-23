62c573dc89167cd4f52e77678236555bba9e884b
pegonzalezspesol <pedro.gonzalez@pesol.es>
2019-10-23 10:19:05 +0200

    [MIG] 13.0 l10n_ch_states

--- l10n_ch_states/__manifest__.py
+++ l10n_ch_states/__manifest__.py
@@ -2,18 +2,13 @@
 # Copyright 2015 Mathias Neef copadoMEDIA UG
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Switzerland Country States',
-    'category': 'Localisation',
-    'summary': '',
-    'version': '12.0.1.0.0',
-    'author': 'copado MEDIA UG,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-switzerland',
-    'license': 'AGPL-3',
-    'depends': [
-        'base',
-    ],
-    'data': [
-        'data/res_country_states.xml'
-    ],
+    "name": "Switzerland Country States",
+    "category": "Localisation",
+    "summary": "",
+    "version": "13.0.1.0.0",
+    "author": "copado MEDIA UG," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-switzerland",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ["data/res_country_states.xml"],
 }
