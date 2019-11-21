4bce5242405d7b00e346bf4184af4a173d55860a
Nooka10 <mails@benoitschopfer.ch>
2019-11-21 09:43:25 +0100

    [11.0][MIG] l10n_ch_pain_direct_debit and l10n_ch_lsv_dd
    I merge the l10n_ch_lsv_dd module into the l10n_ch_pain_direct_debit module.
    correct lint and tests

--- l10n_ch_pain_direct_debit/__manifest__.py
+++ l10n_ch_pain_direct_debit/__manifest__.py
@@ -1,17 +1,29 @@
-# -*- coding: utf-8 -*-
 # © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
+# pylint: disable=C8101
 {
     "name": "Switzerland - PAIN Direct Debit",
     "summary": "Generate ISO 20022 direct debits",
-    "version": "9.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Finance",
-    "author": "Akretion,Camptocamp,Odoo Community Association (OCA)",
+    "author": "Akretion,Camptocamp,Compassion,Odoo Community Association(OCA)",
+    'website': 'http://www.compassion.ch,http://www.braintec-group.com',
     "license": "AGPL-3",
     "depends": [
-        "l10n_ch_pain_base",
         "account_banking_sepa_direct_debit",
+        "l10n_ch_pain_base"
     ],
-    'installable': False,
+    'data': [
+        'data/payment_type.xml',
+        'data/export_filename_sequence.xml',
+        'views/account_payment_line_view.xml',
+        'views/account_payment_mode_view.xml',
+        'views/account_payment_order_view.xml',
+        'views/bank_payment_line_view.xml'
+    ],
+    'demo': [
+        'demo/dd_demo.yml'
+    ],
+    'auto_install': False,
+    'installable': True,
 }
