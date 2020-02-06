6fef080e224e47146af02d431425dc77a0843f9f
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2020-02-06 15:24:10 +0200

    [IMP] l10n_ch_base_bank: apply black

--- l10n_ch_base_bank/models/bank.py
+++ l10n_ch_base_bank/models/bank.py
@@ -1,12 +1,13 @@
 # Copyright 2012-2019 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import re
-from odoo import models, fields, api, _
-from odoo.tools import mod10r
+
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
+from odoo.tools import mod10r
 
 CH_POSTFINANCE_CLEARING = "09000"
-CH_POST_BIC = 'POFICHBEXXX'
+CH_POST_BIC = "POFICHBEXXX"
 
 
 def validate_l10n_ch_postal(postal_acc_number):
@@ -18,13 +19,13 @@ def validate_l10n_ch_postal(postal_acc_number):
     """
     if not postal_acc_number:
         raise ValidationError(_("There is no postal account number."))
-    if re.match('^[0-9]{2}-[0-9]{1,6}-[0-9]$', postal_acc_number):
-        ref_subparts = postal_acc_number.split('-')
+    if re.match("^[0-9]{2}-[0-9]{1,6}-[0-9]$", postal_acc_number):
+        ref_subparts = postal_acc_number.split("-")
         postal_acc_number = (
-            ref_subparts[0] + ref_subparts[1].rjust(6, '0') + ref_subparts[2]
+            ref_subparts[0] + ref_subparts[1].rjust(6, "0") + ref_subparts[2]
         )
 
-    if not re.match(r'\d{9}$', postal_acc_number or ''):
+    if not re.match(r"\d{9}$", postal_acc_number or ""):
         msg = _("The postal does not match 9 digits position.")
         raise ValidationError(msg)
 
@@ -38,13 +39,13 @@ def pretty_l10n_ch_postal(number):
     as per specifications with '-' separators.
     eg. 010000628 -> 01-162-8
     """
-    if re.match('^[0-9]{2}-[0-9]{1,6}-[0-9]$', number or ''):
+    if re.match("^[0-9]{2}-[0-9]{1,6}-[0-9]$", number or ""):
         return number
     currency_code = number[:2]
     middle_part = number[2:-1]
     trailing_cipher = number[-1]
-    middle_part = re.sub('^0*', '', middle_part)
-    return currency_code + '-' + middle_part + '-' + trailing_cipher
+    middle_part = re.sub("^0*", "", middle_part)
+    return currency_code + "-" + middle_part + "-" + trailing_cipher
 
 
 def _is_l10n_ch_postfinance_iban(iban):
@@ -52,30 +53,22 @@ def _is_l10n_ch_postfinance_iban(iban):
     CHXX 0900 0XXX XXXX XXXX K
     Where 09000 is the clearing number
     """
-    return (iban.startswith('CH')
-            and iban[4:9] == CH_POSTFINANCE_CLEARING)
+    return iban.startswith("CH") and iban[4:9] == CH_POSTFINANCE_CLEARING
 
 
 class Bank(models.Model):
     """Inherit res.bank class in order to add swiss specific field"""
-    _inherit = 'res.bank'
 
-    code = fields.Char(
-        string='Code',
-        help='Internal reference'
-    )
+    _inherit = "res.bank"
+
+    code = fields.Char(string="Code", help="Internal reference")
     clearing = fields.Char(
-        string='Clearing number',
-        help='Swiss unique bank identifier also used in IBAN number'
+        string="Clearing number",
+        help="Swiss unique bank identifier also used in IBAN number",
     )
-    city = fields.Char(
-        string='City',
-        help="City of the bank",
-    )
+    city = fields.Char(string="City", help="City of the bank")
     country_code = fields.Char(
-        string="Country code",
-        related="country.code",
-        readonly=True,
+        string="Country code", related="country.code", readonly=True
     )
 
     def is_swiss_post(self):
@@ -84,41 +77,31 @@ class Bank(models.Model):
     def name_get(self):
         """Format displayed name"""
         res = []
-        cols = ('bic', 'name', 'street', 'city')
+        cols = ("bic", "name", "street", "city")
         for bank in self:
             vals = (bank[x] for x in cols if bank[x])
-            res.append((bank.id, ' - '.join(vals)))
+            res.append((bank.id, " - ".join(vals)))
         return res
 
     @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=80):
+    def name_search(self, name, args=None, operator="ilike", limit=80):
         """Extends to look on bank code, bic, name, street and city"""
         if args is None:
             args = []
         ids = []
-        cols = ('code', 'bic', 'name', 'street', 'city')
+        cols = ("code", "bic", "name", "street", "city")
         if name:
-            for val in name.split(' '):
+            for val in name.split(" "):
                 for col in cols:
-                    tmp_ids = self.search(
-                        [(col, 'ilike', val)] + args,
-                        limit=limit
-                    )
+                    tmp_ids = self.search([(col, "ilike", val)] + args, limit=limit)
                     if tmp_ids:
                         ids += tmp_ids.ids
                         break
         else:
-            ids = self.search(
-                args,
-                limit=limit
-            ).ids
+            ids = self.search(args, limit=limit).ids
         # we sort by occurrence
         to_ret_ids = list(set(ids))
-        to_ret_ids = sorted(
-            to_ret_ids,
-            key=lambda x: ids.count(x),
-            reverse=True
-        )
+        to_ret_ids = sorted(to_ret_ids, key=lambda x: ids.count(x), reverse=True)
         return self.browse(to_ret_ids).name_get()
 
 
@@ -147,9 +130,10 @@ class ResPartnerBank(models.Model):
     acc_number in this case recomputed by as partner_name + l10n_ch_postal
 
     """
-    _inherit = 'res.partner.bank'
 
-    @api.onchange('acc_number')
+    _inherit = "res.partner.bank"
+
+    @api.onchange("acc_number")
     def onchange_acc_number_set_swiss_bank(self):
         """ Deduce information from IBAN
         and set postal number when undefined
@@ -164,31 +148,26 @@ class ResPartnerBank(models.Model):
 
         bank = self.bank_id
         postal = self.l10n_ch_postal
-        if self.acc_type == 'iban':
+        if self.acc_type == "iban":
             if not bank:
                 bank = self._get_ch_bank_from_iban()
-            postal = self._retrieve_l10n_ch_postal(
-                self.sanitized_acc_number
-            )
-        elif self.acc_type == 'postal':
+            postal = self._retrieve_l10n_ch_postal(self.sanitized_acc_number)
+        elif self.acc_type == "postal":
             postal = self.acc_number
 
         self.bank_id = bank
         self.l10n_ch_postal = postal
 
-    @api.constrains(
-        'l10n_ch_isr_subscription_chf',
-        'l10n_ch_isr_subscription_eur'
-    )
+    @api.constrains("l10n_ch_isr_subscription_chf", "l10n_ch_isr_subscription_eur")
     def _check_subscription_num(self):
         for rec in self:
-            for currency in ['chf', 'eur']:
-                field_name = 'l10n_ch_isr_subscription_{}'.format(currency)
+            for currency in ["chf", "eur"]:
+                field_name = "l10n_ch_isr_subscription_{}".format(currency)
                 if rec[field_name]:
                     validate_l10n_ch_postal(rec[field_name])
         return True
 
-    @api.constrains('l10n_ch_postal')
+    @api.constrains("l10n_ch_postal")
     def _check_postal_num(self):
         """Validate postal number format"""
         for rec in self:
@@ -200,35 +179,35 @@ class ResPartnerBank(models.Model):
         """Check if number generated from postal number, if yes replace it on
         new """
         part_name = self.partner_id.name
-        if not part_name and self.env.context.get('default_partner_id'):
-            partner_id = self.env.context.get('default_partner_id')
-            part_name = self.env['res.partner'].browse(partner_id)[0].name
+        if not part_name and self.env.context.get("default_partner_id"):
+            partner_id = self.env.context.get("default_partner_id")
+            part_name = self.env["res.partner"].browse(partner_id)[0].name
         self.acc_number = self._compute_name_from_postal_number(
-            part_name, self.l10n_ch_postal)
+            part_name, self.l10n_ch_postal
+        )
 
     @api.model
     def _compute_name_from_postal_number(self, partner_name, postal_number):
         """This method makes sure to generate a unique name"""
         if partner_name and postal_number:
-            acc_name = _("{}/Postal number {}").format(
-                partner_name,
-                postal_number
-            )
+            acc_name = _("{}/Postal number {}").format(partner_name, postal_number)
         elif postal_number:
             acc_name = _("Postal number {}").format(postal_number)
         else:
-            return ''
+            return ""
 
-        exist_count = self.env['res.partner.bank'].search_count(
-            [('acc_number', '=like', acc_name)])
+        exist_count = self.env["res.partner.bank"].search_count(
+            [("acc_number", "=like", acc_name)]
+        )
         # if acc_number not unique iterate on bank_accounts while not get
         # unique number
         if exist_count:
             name_exist = exist_count
             while name_exist:
                 new_name = acc_name + " #{}".format(exist_count)
-                name_exist = self.env['res.partner.bank'].search_count(
-                    [('acc_number', '=', new_name)])
+                name_exist = self.env["res.partner.bank"].search_count(
+                    [("acc_number", "=", new_name)]
+                )
                 exist_count += 1
             acc_name = new_name
         return acc_name
@@ -240,23 +219,23 @@ class ResPartnerBank(models.Model):
         mandatory when we have postal number, so we compute acc_number in
         onchange methods and check it here also
         """
-        if not vals.get('acc_number') and vals.get('l10n_ch_postal'):
-            partner = self.env['res.partner'].browse(vals.get('partner_id'))
-            vals['acc_number'] = self._compute_name_from_postal_number(
-                partner.name,
-                vals['l10n_ch_postal']
+        if not vals.get("acc_number") and vals.get("l10n_ch_postal"):
+            partner = self.env["res.partner"].browse(vals.get("partner_id"))
+            vals["acc_number"] = self._compute_name_from_postal_number(
+                partner.name, vals["l10n_ch_postal"]
             )
         return super().create(vals)
 
     def _get_ch_bank_from_iban(self):
         """Extract clearing number from CH iban to find the bank"""
-        if self.acc_type != 'iban' and self.acc_number[:2] != 'CH':
+        if self.acc_type != "iban" and self.acc_number[:2] != "CH":
             return False
         clearing = self.sanitized_acc_number[4:9]
-        return clearing and self.env['res.bank'].search(
-            [('clearing', '=', clearing)], limit=1)
+        return clearing and self.env["res.bank"].search(
+            [("clearing", "=", clearing)], limit=1
+        )
 
-    @api.onchange('l10n_ch_postal')
+    @api.onchange("l10n_ch_postal")
     def onchange_l10n_ch_postal_set_acc_number(self):
         """
         If postal number changes and it's a postal bank update acc_number to
@@ -264,36 +243,36 @@ class ResPartnerBank(models.Model):
         possibility set it manually and also avoid to shadow other logic on
         acc_number if exist
         """
-        if self.acc_type == 'iban':
+        if self.acc_type == "iban":
             return
 
         if self.l10n_ch_postal:
-            if self.acc_type == 'postal':
+            if self.acc_type == "postal":
                 # flash bank if it was previously setup, also trigger acc_type
                 # changing
-                self.bank_id = ''
+                self.bank_id = ""
             self._update_acc_name()
         else:
             # flash bank if it was previously setup
-            self.acc_number = ''
+            self.acc_number = ""
 
         if self.bank_id.is_swiss_post():
             self.acc_number = self.l10n_ch_postal
             return
 
-    @api.onchange('bank_id')
+    @api.onchange("bank_id")
     def onchange_bank_set_acc_number(self):
         # Track bank change to update acc_name if needed
-        if not self.bank_id or self.acc_type == 'iban':
+        if not self.bank_id or self.acc_type == "iban":
             return
         if self.bank_id.is_swiss_post():
             self.acc_number = self.l10n_ch_postal
         else:
             self._update_acc_name()
 
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def onchange_partner_set_acc_number(self):
         # When acc_number was computed automatically we call regeneration
         # as partner name is part of acc_number
-        if self.acc_type == 'bank' and self.l10n_ch_postal:
+        if self.acc_type == "bank" and self.l10n_ch_postal:
             self._update_acc_name()
--- l10n_ch_base_bank/models/invoice.py
+++ l10n_ch_base_bank/models/invoice.py
@@ -2,32 +2,44 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import re
 
-from odoo import models, api, _
+from odoo import _, api, exceptions, models
 from odoo.tools import mod10r
-from odoo import exceptions
 
 
 class AccountMove(models.Model):
 
     _inherit = "account.move"
 
-    def _search(self, args, offset=0, limit=None, order=None, count=False,
-                access_rights_uid=None):
+    def _search(
+        self,
+        args,
+        offset=0,
+        limit=None,
+        order=None,
+        count=False,
+        access_rights_uid=None,
+    ):
         domain = []
         for arg in args:
             if not isinstance(arg, (tuple, list)) or len(arg) != 3:
                 domain.append(arg)
                 continue
             field, operator, value = arg
-            if field != 'ref':
+            if field != "ref":
                 domain.append(arg)
                 continue
-            if operator not in ('like', 'ilike', '=like', '=ilike',
-                                'not like', 'not ilike'):
+            if operator not in (
+                "like",
+                "ilike",
+                "=like",
+                "=ilike",
+                "not like",
+                "not ilike",
+            ):
                 domain.append(arg)
                 continue
             if value:
-                value = value.replace(' ', '')
+                value = value.replace(" ", "")
                 if not value:
                     # original value contains only spaces, the query
                     # would return all rows, so avoid a costly search
@@ -35,46 +47,62 @@ class AccountMove(models.Model):
                     continue
                 # add wildcards for the like search, except if the operator
                 # is =like of =ilike because they are supposed to be there yet
-                if operator.startswith('='):
+                if operator.startswith("="):
                     operator = operator[1:]
                 else:
-                    value = '%%%s%%' % (value,)
+                    value = "%{}%".format(value)
             # add filtered operator to query
-            query_op = ("SELECT id FROM account_move "
-                        "WHERE REPLACE(ref, ' ', '') %s %%s" %
-                        (operator,))
+            query_op = (
+                "SELECT id FROM account_move "
+                "WHERE REPLACE(ref, ' ', '') %s %%s" % (operator,)
+            )
             # avoid pylint check on no-sql-injection query_op is safe
             query = query_op
             self.env.cr.execute(query, (value,))
             ids = [t[0] for t in self.env.cr.fetchall()]
-            domain.append(('id', 'in', ids))
+            domain.append(("id", "in", ids))
 
         return super()._search(
-            domain, offset=offset, limit=limit, order=order, count=count,
-            access_rights_uid=access_rights_uid)
+            domain,
+            offset=offset,
+            limit=limit,
+            order=order,
+            count=count,
+            access_rights_uid=access_rights_uid,
+        )
 
-    @api.constrains('ref')
+    @api.constrains("ref")
     def _check_bank_type_for_type_isr(self):
         for move in self:
-            if move.type == 'out_invoice' and move._is_isr_ref():
-                if hasattr(super(), 'partner_banks_to_show'):
+            if move.type == "out_invoice" and move._is_isr_ref():
+                if hasattr(super(), "partner_banks_to_show"):
                     bank_acc = move.partner_banks_to_show()
                 else:
                     bank_acc = move.invoice_partner_bank_id
                 if not bank_acc:
                     raise exceptions.ValidationError(
-                        _("Bank account shouldn't be empty, for ISR ref"
-                          " type, you can set it manually or set appropriate"
-                          " payment mode.")
+                        _(
+                            "Bank account shouldn't be empty, for ISR ref"
+                            " type, you can set it manually or set appropriate"
+                            " payment mode."
+                        )
                     )
-                if (bank_acc.acc_type != 'qr-iban'
-                        and (move.currency_id.name == 'CHF'
-                             and not bank_acc.l10n_ch_isr_subscription_chf)
-                        or (move.currency_id.name == 'EUR'
-                            and not bank_acc.l10n_ch_isr_subscription_eur)):
+                if (
+                    bank_acc.acc_type != "qr-iban"
+                    and (
+                        move.currency_id.name == "CHF"
+                        and not bank_acc.l10n_ch_isr_subscription_chf
+                    )
+                    or (
+                        move.currency_id.name == "EUR"
+                        and not bank_acc.l10n_ch_isr_subscription_eur
+                    )
+                ):
                     raise exceptions.ValidationError(
-                        _("Bank account must contain a subscription number for"
-                          " ISR ref type.")
+                        _(
+                            "Bank account must contain a subscription number for"
+                            " ISR ref type."
+                        )
                     )
         return True
 
@@ -90,8 +118,8 @@ class AccountMove(models.Model):
         """
         if not self.ref:
             return False
-        if re.match(r'^(\d{27}|\d{2}( \d{5}){5})$', self.ref):
-            ref = self.ref.replace(' ', '')
+        if re.match(r"^(\d{27}|\d{2}( \d{5}){5})$", self.ref):
+            ref = self.ref.replace(" ", "")
             return ref == mod10r(ref[:-1])
         return False
 
