67be3e02ca3a344438614fad7321080d182e65c2
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2020-02-06 15:24:10 +0200

    [IMP] l10n_ch_base_bank: apply black

--- l10n_ch_base_bank/tests/test_bank_type.py
+++ l10n_ch_base_bank/tests/test_bank_type.py
@@ -4,37 +4,35 @@ from odoo.tests import common
 
 
 class TestBankType(common.SavepointCase):
-
     def test_is_bank_account_with_post(self):
-        bank = self.env['res.bank'].create({
-            'name': 'BCV',
-            'bic': 'BCVLCH2LXXX',
-            'clearing': '234234',
-        })
-        bank_account = self.env['res.partner.bank'].create({
-
-            'partner_id': self.partner.id,
-            'bank_id': bank.id,
-            'acc_number': 'Bank/Postal Nr 01-1234-1',
-        })
-        self.assertEqual(bank_account.acc_type, 'bank')
+        bank = self.env["res.bank"].create(
+            {"name": "BCV", "bic": "BCVLCH2LXXX", "clearing": "234234"}
+        )
+        bank_account = self.env["res.partner.bank"].create(
+            {
+                "partner_id": self.partner.id,
+                "bank_id": bank.id,
+                "acc_number": "Bank/Postal Nr 01-1234-1",
+            }
+        )
+        self.assertEqual(bank_account.acc_type, "bank")
 
     def test_is_postal_account(self):
-        bank = self.env['res.bank'].create({
-            'name': 'BCV',
-            'bic': 'BCVLCH2LXXX',
-            'clearing': '234234',
-        })
-        bank_account = self.env['res.partner.bank'].create({
-            'partner_id': self.partner.id,
-            'bank_id': bank.id,
-            'acc_number': '01-1234-1',
-        })
-        self.assertEqual(bank_account.acc_type, 'postal')
+        bank = self.env["res.bank"].create(
+            {"name": "BCV", "bic": "BCVLCH2LXXX", "clearing": "234234"}
+        )
+        bank_account = self.env["res.partner.bank"].create(
+            {
+                "partner_id": self.partner.id,
+                "bank_id": bank.id,
+                "acc_number": "01-1234-1",
+            }
+        )
+        self.assertEqual(bank_account.acc_type, "postal")
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        cls.company = cls.env.ref('base.main_company')
-        cls.partner = cls.env.ref('base.main_partner')
+        cls.company = cls.env.ref("base.main_company")
+        cls.partner = cls.env.ref("base.main_partner")
--- l10n_ch_base_bank/tests/test_create_invoice.py
+++ l10n_ch_base_bank/tests/test_create_invoice.py
@@ -1,46 +1,37 @@
 # Copyright 2012-2019 Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo.tests import common
 from odoo import exceptions
+from odoo.tests import common
 from odoo.tests.common import Form
 
 
 class TestCreateMove(common.SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        cls.company = cls.env.ref('base.main_company')
-        cls.partner = cls.env.ref('base.res_partner_12')
-        bank = cls.env['res.bank'].create({
-            'name': 'BCV',
-            'bic': 'BBRUBEBB',
-            'clearing': '234234',
-        })
+        cls.company = cls.env.ref("base.main_company")
+        cls.partner = cls.env.ref("base.res_partner_12")
+        bank = cls.env["res.bank"].create(
+            {"name": "BCV", "bic": "BBRUBEBB", "clearing": "234234"}
+        )
         # define company bank account
-        cls.bank_journal = cls.env['account.journal'].create({
-            'company_id': cls.company.id,
-            'type': 'bank',
-            'code': 'BNK42',
-            'bank_id': bank.id,
-            'bank_acc_number': '01-1234-1',
-        })
+        cls.bank_journal = cls.env["account.journal"].create(
+            {
+                "company_id": cls.company.id,
+                "type": "bank",
+                "code": "BNK42",
+                "bank_id": bank.id,
+                "bank_acc_number": "01-1234-1",
+            }
+        )
         cls.bank_acc = cls.bank_journal.bank_account_id
-        cls.bank_acc.write({
-            'l10n_ch_isr_subscription_chf': '01-162-8',
-            'sequence': 1,
-        })
-        fields_list = [
-            'company_id',
-            'user_id',
-            'currency_id',
-            'journal_id',
-        ]
-        cls.inv_values = cls.env['account.move'].default_get(fields_list)
+        cls.bank_acc.write({"l10n_ch_isr_subscription_chf": "01-162-8", "sequence": 1})
+        fields_list = ["company_id", "user_id", "currency_id", "journal_id"]
+        cls.inv_values = cls.env["account.move"].default_get(fields_list)
 
     def new_form(self):
-        inv = Form(self.env['account.move'].with_context(default_type='out_invoice'))
+        inv = Form(self.env["account.move"].with_context(default_type="out_invoice"))
         # Form(
         #     self.env['account.move'],
         #     view='account.view_move_form'
@@ -54,7 +45,7 @@ class TestCreateMove(common.SavepointCase):
         move = inv_form.save()
         self.assertFalse(move._is_isr_ref())
 
-        inv_form.ref = '132000000000000000000000014'
+        inv_form.ref = "132000000000000000000000014"
         inv_form.save()
         self.assertTrue(move._is_isr_ref())
 
@@ -64,7 +55,7 @@ class TestCreateMove(common.SavepointCase):
 
         self.assertFalse(move._is_isr_ref())
 
-        move.ref = '132000000000004'
+        move.ref = "132000000000004"
         # We consider such ref to be unstructured
         # and we shouldn't generate such refs
         self.assertFalse(move._is_isr_ref())
@@ -78,7 +69,7 @@ class TestCreateMove(common.SavepointCase):
 
         self.assertFalse(move._is_isr_ref())
 
-        move.ref = 'Not a ISR ref with 27 chars'
+        move.ref = "Not a ISR ref with 27 chars"
         self.assertFalse(move._is_isr_ref())
 
     def test_emit_move_with_missing_isr_ref(self):
@@ -97,17 +88,7 @@ class TestCreateMove(common.SavepointCase):
         self.assertFalse(move._is_isr_ref())
         inv_form.invoice_partner_bank_id = False
         with self.assertRaises(exceptions.ValidationError):
-            inv_form.ref = '132000000000000000000000014'
-            inv_form.save()
-
-    def test_emit_move_with_isr_ref_missing_subscr_num(self):
-        inv_form = self.new_form()
-        move = inv_form.save()
-        inv_form.save()
-        self.assertFalse(move._is_isr_ref())
-        self.bank_acc.l10n_ch_isr_subscription_chf = False
-        with self.assertRaises(exceptions.ValidationError):
-            inv_form.ref = '132000000000000000000000014'
+            inv_form.ref = "132000000000000000000000014"
             inv_form.save()
 
     def test_emit_move_with_isr_ref_subscr_num_wrong_currency(self):
@@ -115,7 +96,7 @@ class TestCreateMove(common.SavepointCase):
         move = inv_form.save()
         inv_form.save()
         self.assertFalse(move._is_isr_ref())
-        move.currency_id = self.env.ref('base.EUR')
+        move.currency_id = self.env.ref("base.EUR")
         with self.assertRaises(exceptions.ValidationError):
-            inv_form.ref = '132000000000000000000000014'
+            inv_form.ref = "132000000000000000000000014"
             inv_form.save()
