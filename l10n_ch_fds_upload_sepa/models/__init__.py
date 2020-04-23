a4d64920082a9dddb0adcec64dfb82c85015fea2
Philipp Hug <philipp@hug.cx>
2020-04-23 13:12:42 +0200

    [11.0][MIG] Update to Odoo 11

--- l10n_ch_fds_upload_sepa/models/__init__.py
+++ l10n_ch_fds_upload_sepa/models/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 from . import fds_postfinance_account_sepa
 from . import fds_sepa_upload_history
--- l10n_ch_fds_upload_sepa/wizards/__init__.py
+++ l10n_ch_fds_upload_sepa/wizards/__init__.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
 
 from . import payment_order_upload_sepa_wizard
