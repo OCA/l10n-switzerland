d30acee56b6816ace8288ef486cdd02a47d1e948
Philipp Hug <philipp@hug.cx>
2020-04-23 15:57:36 +0200

    [12.0][MIG] Migrate l10n_ch_import_pain002

--- l10n_ch_import_pain002/__manifest__.py
+++ l10n_ch_import_pain002/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # © 2017-2018 Compassion CH (Marco Monzione)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Import pain002',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Monzione Marco, Odoo Community Association (OCA)',
     'website': 'https://www.compassion.ch',
--- l10n_ch_import_pain002/tests/test_import_pain002.py
+++ l10n_ch_import_pain002/tests/test_import_pain002.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from odoo.tests import TransactionCase
 from odoo.modules import get_module_resource
 
