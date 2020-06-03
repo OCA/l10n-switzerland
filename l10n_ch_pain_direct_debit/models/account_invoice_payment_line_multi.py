d9d486994df1541740152e50777fb550f9bb5326
qgigon <gigon.quentin@gmail.com>
2020-06-03 10:32:36 +0200

    updated comment at beginning of file

--- l10n_ch_pain_direct_debit/models/account_invoice_payment_line_multi.py
+++ l10n_ch_pain_direct_debit/models/account_invoice_payment_line_multi.py
@@ -1,4 +1,4 @@
-# © 2016 Akretion (<https://www.akretion.com>)
+# © 2020 Quentin Gigon <gigon.quentin@gmail.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, api
