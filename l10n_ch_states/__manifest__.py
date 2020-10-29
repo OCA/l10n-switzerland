bf965a4b9a989f068a57b6d55526933a580a213f
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2020-10-29 18:03:32 +0100

    [MIG] l10n_ch_states: Migration to 14.0

--- l10n_ch_states/__manifest__.py
+++ l10n_ch_states/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2019 Camptocamp SA
+# Copyright 2019-2020 Camptocamp SA
 # Copyright 2015 Mathias Neef copadoMEDIA UG
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Switzerland Country States",
     "category": "Localisation",
     "summary": "",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "copado MEDIA UG," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-switzerland",
     "license": "AGPL-3",
