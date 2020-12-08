7f9d633bfc2d4b0352f57f3ced36ce94f894a965
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2020-12-08 13:56:10 +0100

    [13.0][ADD] ebill_paynet_account_financial_discount
    This module implements the discount information in the payment terms node
    of the Paynet message. In regards to the module `account_financial_discount`.
    
    When the discount information is present in the payment terms of an invoice, they
    will automatically be included in the payment term of the Paynet message.

---
+++ ebill_paynet_account_financial_discount/models/paynet_invoice_message.py
@@ -0,0 +1,31 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import os
+
+from odoo import models
+from odoo.modules.module import get_module_root
+
+MODULE_PATH = get_module_root(os.path.dirname(__file__))
+DISCOUNT_TEMPLATE = "discount-2003A.xml.jinja"
+TEMPLATE_DIR = [MODULE_PATH + "/messages"]
+
+
+class PaynetInvoiceMessage(models.Model):
+    _inherit = "paynet.invoice.message"
+
+    def _get_jinja_env(self, template_dir):
+        template_dir += TEMPLATE_DIR
+        return super()._get_jinja_env(template_dir)
+
+    def _get_payload_params(self):
+        params = super()._get_payload_params()
+        params["discount_template"] = DISCOUNT_TEMPLATE
+        discount = {}
+        if self.invoice_id.invoice_payment_term_id and self.invoice_id.invoice_payment_term_id.percent_discount:
+            discount = {
+                "percentage": self.invoice_id.invoice_payment_term_id.percent_discount,
+                "days": self.invoice_id.invoice_payment_term_id.days_discount
+            }
+        params["discount"] = discount
+        return params
