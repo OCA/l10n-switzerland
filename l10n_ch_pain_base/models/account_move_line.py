210558717296123dda94a2ee406fadc467f0414f
Simone Rubino <simone.rubino@agilebg.com>
2021-09-28 09:24:47 +0200

    In QR-IBAN case, the QR reference must be communicated (#1)

--- l10n_ch_pain_base/models/account_move_line.py
+++ l10n_ch_pain_base/models/account_move_line.py
@@ -12,6 +12,7 @@ class AccountMoveLine(models.Model):
         if self.move_id.partner_bank_id and self.move_id._has_isr_ref():
             if self.move_id.partner_bank_id._is_qr_iban():
                 vals["communication_type"] = "qrr"
+                vals["communication"] = self.move_id.payment_reference
             else:
                 vals["local_instrument"] = "CH01"
                 vals["communication_type"] = "isr"
