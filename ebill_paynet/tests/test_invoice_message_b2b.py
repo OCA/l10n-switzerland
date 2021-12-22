b5f881a2020eb4594b433eab17511fcd566ff81a
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2021-12-23 01:34:38 +0200

    ebill_paynet: Add rounding on tax group

--- ebill_paynet/tests/test_invoice_message_b2b.py
+++ ebill_paynet/tests/test_invoice_message_b2b.py
@@ -3,11 +3,14 @@
 
 from string import Template
 
+import mock
 from freezegun import freeze_time
 
 from odoo.tools import file_open
 
 from .common import CommonCase
+
+EBILL = "odoo.addons.ebill_paynet.models.paynet_invoice_message.PaynetInvoiceMessage."
 
 
 @freeze_time("2019-06-21 09:06:00")
@@ -64,6 +67,62 @@ class TestInvoiceMessage(CommonCase):
         # Prepare the XML file that is expected
         expected_tmpl = Template(
             file_open("ebill_paynet/tests/examples/invoice_isr_b2b.xml").read()
+        )
+        expected = expected_tmpl.substitute(IC_REF=message.ic_ref).encode("utf8")
+        # Remove the comments in the expected xml
+        expected_nocomment = [
+            line
+            for line in expected.split(b"\n")
+            if not line.lstrip().startswith(b"<!--")
+        ]
+        expected_nocomment = b"\n".join(expected_nocomment)
+        self.assertFalse(self.compare_xml_line_by_line(payload, expected_nocomment))
+        self.assertXmlEquivalentOutputs(payload, expected_nocomment)
+
+    @mock.patch(EBILL + "_get_ic_ref")
+    def test_invoice_qr_rounding(self, get_ic_ref):
+        """ Test case to check float rounding fits well"""
+        get_ic_ref.return_value = "SA000000000003"
+        invoice = self.env["account.move"].create(
+            {
+                "name": "INV_TEST_01",
+                "partner_id": self.customer.id,
+                "invoice_date_due": "2019-07-01",
+                "type": "out_invoice",
+                "invoice_line_ids": [
+                    (0, 0, {"name": "A little note", "display_type": "line_note"}),
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "Phone support",
+                            "quantity": 1.0,
+                            "price_unit": 4.5,
+                            "account_id": self.at_receivable.id,
+                            "tax_ids": [(4, self.tax7.id, 0)],
+                        },
+                    ),
+                ],
+            }
+        )
+        invoice.action_post()
+        invoice.invoice_payment_ref = "1234567890"
+        invoice.invoice_partner_bank_id = self.partner_bank.id
+        # amount inside is not rounded
+        self.assertEqual(invoice.amount_by_group[0][1], 0.35000000000000003)
+        message = invoice.create_paynet_message()
+        message.payload = message._generate_payload()
+        # Remove the PDF file data from the XML to ease testing
+        lines = message.payload.splitlines()
+        for pos, line in enumerate(lines):
+            if line.find("Back-Pack") != -1:
+                lines.pop(pos)
+                break
+        payload = "\n".join(lines).encode("utf8")
+        self.assertXmlDocument(payload)
+        # Prepare the XML file that is expected
+        expected_tmpl = Template(
+            file_open("ebill_paynet/tests/examples/invoice_qr_b2b_rounded.xml").read()
         )
         expected = expected_tmpl.substitute(IC_REF=message.ic_ref).encode("utf8")
         # Remove the comments in the expected xml
