fb1f817b6d8eeb6f125a64069a0ac32076b96a02
Holger Brunn <mail@hunki-enterprises.com>
2022-01-17 12:20:29 +0100

    [IMP] l10n_ch_pain_*: black, isort, prettier

--- l10n_ch_pain_base/models/account_payment_line.py
+++ l10n_ch_pain_base/models/account_payment_line.py
@@ -1,18 +1,18 @@
 # copyright 2016 Akretion - Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountPaymentLine(models.Model):
-    _inherit = 'account.payment.line'
+    _inherit = "account.payment.line"
 
-    local_instrument = fields.Selection(
-        selection_add=[('CH01', 'CH01 (ISR)')])
-    communication_type = fields.Selection(selection_add=[
-        ('isr', 'ISR'), ('qrr', 'QRR')])
+    local_instrument = fields.Selection(selection_add=[("CH01", "CH01 (ISR)")])
+    communication_type = fields.Selection(
+        selection_add=[("isr", "ISR"), ("qrr", "QRR")]
+    )
 
     def invoice_reference_type2communication_type(self):
         res = super().invoice_reference_type2communication_type()
-        res['isr'] = 'isr'
+        res["isr"] = "isr"
         return res
