e362ca2abcb2e7d4d051ce5f6af1266b0a84b53b
Akim Juillerat <akim.juillerat@camptocamp.com>
2022-05-09 14:09:13 +0200

    Add module l10n_ch_qr_no_amount

---
+++ l10n_ch_qr_no_amount/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Switzerland - No Amount QR-bill",
+    "summary": "Allow to print QR bill without amount",
+    "version": "13.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Localization",
+    "website": "https://github.com/OCA/l10n-switzerland",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "maintainers": ["grindtildeath"],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["l10n_ch"],
+    "data": ["report/swissqr_no_amount_report.xml"],
+}
