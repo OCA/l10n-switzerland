ced64ac30cfd4b5974a672eb611219a2156c1737
Frédéric Siffert <frederic@treyvaud.ch>
2022-05-25 10:44:00 +0200

    Create __manifest__.py

---
+++ l10n_ch_isrs/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Switzerland - isrs",
+    'author': 'Caravanes Treyvaud S.A.',
+    'description': """
+Swiss ISR 04
+============
+This module modify the generation of ISR to
+ISR without amount if payment have many therms
+    """,
+    'version': '15.0',
+    'category': 'Accounting/Localizations',
+    'depends': ['l10n_ch'],
+    'license': 'AGPL-3',
+}
