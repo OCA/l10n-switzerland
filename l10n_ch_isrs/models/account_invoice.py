88bfd0c565b0e129bd87fe6f22b762fb913028d4
Frédéric Siffert <frederic@treyvaud.ch>
2022-05-25 11:12:29 +0200

    Update account_invoice.py

--- l10n_ch_isrs/models/account_invoice.py
+++ l10n_ch_isrs/models/account_invoice.py
@@ -1,18 +1,16 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, models, _
+from odoo import models
 
 class AccountMove(models.Model):
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
     def _get_l10n_ch_isr_optical_amount(self):
-        if len(self.invoice_payment_term_id.line_ids)>1:
+        if len(self.invoice_payment_term_id.line_ids) > 1:
             return "042"
         else:
             return super()._get_l10n_ch_isr_optical_amount()
 
     def split_total_amount(self):
-        if len(self.invoice_payment_term_id.line_ids)>1:
-            return ["",""]
+        if len(self.invoice_payment_term_id.line_ids) > 1:
+            return ["", ""]
         else:
             return super().split_total_amount()
