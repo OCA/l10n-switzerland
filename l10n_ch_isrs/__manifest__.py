ba44cf5bb7f2bf977b3bbc1b54572bb305e2eb23
Frédéric Siffert <frederic@treyvaud.ch>
2022-05-25 11:20:23 +0200

    Update __manifest__.py

--- l10n_ch_isrs/__manifest__.py
+++ l10n_ch_isrs/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name': "Switzerland - ISRs 04 (isr without amount)",
     'summary': "print ISRs without amount if payment have many therms"
-    'author': "Caravanes Treyvaud S.A., Odoo Community Association (OCA)",
+    'author': "Caravanes Treyvaud SA, Odoo Community Association (OCA)",
     'version': "15.0.0.1.0",
     'category': "Accounting/Localizations",
     'depends': ["l10n_ch"],
