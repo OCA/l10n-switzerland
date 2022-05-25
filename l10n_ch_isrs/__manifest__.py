b1899b59fdcb68ff4c160df34645833165a1afdc
Frédéric Siffert <frederic@treyvaud.ch>
2022-05-25 11:31:30 +0200

    Update __manifest__.py

--- l10n_ch_isrs/__manifest__.py
+++ l10n_ch_isrs/__manifest__.py
@@ -1,10 +1,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': "Switzerland - ISRs 04 (isr without amount)",
-    'summary': "print ISRs without amount if payment have many therms",
-    'author': "Caravanes Treyvaud SA, Odoo Community Association (OCA)",
-    'version': "15.0.0.1.0",
-    'category': "Accounting/Localizations",
-    'depends': ["l10n_ch"],
-    'license': "AGPL-3",
+    "name": "Switzerland - ISRs 04 (isr without amount)",
+    "summary": "print ISRs without amount if payment have many therms",
+    "author": "Caravanes Treyvaud SA, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/",
+    "version": "15.0.0.1.0",
+    "category": "Accounting/Localizations",
+    "depends": ["l10n_ch"],
+    "license": "AGPL-3",
 }
