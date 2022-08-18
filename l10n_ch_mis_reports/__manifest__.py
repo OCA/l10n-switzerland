a162a39e284a10e9ac99d4b39f141b95425759c1
Alberto Re <alberto.re@agilebg.com>
2022-08-18 15:12:15 +0200

    [IMP] l10n_ch_mis_reports: pre-commit execution

--- l10n_ch_mis_reports/__manifest__.py
+++ l10n_ch_mis_reports/__manifest__.py
@@ -3,22 +3,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Switzerland - MIS reports',
-    'summary': 'Specific MIS reports for switzerland localization',
-    'version': '12.0.1.0.1',
-    'author': "Camptocamp,Odoo Community Association (OCA)",
-    'category': 'Localization',
-    'website': 'https://github.com/OCA/l10n-switzerland',
-    'license': 'AGPL-3',
-    'depends': [
-        'l10n_ch',
-        'mis_builder',
-        'mis_builder_budget',
-        'l10n_ch_account_tags',
+    "name": "Switzerland - MIS reports",
+    "summary": "Specific MIS reports for switzerland localization",
+    "version": "12.0.1.0.1",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "category": "Localization",
+    "website": "https://github.com/OCA/l10n-switzerland",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_ch",
+        "mis_builder",
+        "mis_builder_budget",
+        "l10n_ch_account_tags",
     ],
-    'data': [
-        'data/mis.report.style.csv',
-        'data/mis.report.csv',
-        'data/mis.report.kpi.csv',
+    "data": [
+        "data/mis.report.style.csv",
+        "data/mis.report.csv",
+        "data/mis.report.kpi.csv",
     ],
 }
