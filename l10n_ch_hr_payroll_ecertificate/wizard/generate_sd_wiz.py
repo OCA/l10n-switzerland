3732011ef8d931a5f8a96a24d131b2dc5ee9fe9e
Simon <simon.gonzalez@bluewin.ch>
2023-04-17 08:39:07 +0200

    [MIG] l10n_ch_hr_payroll_ecertificate: Migration to 13.0 Migration task from OCA for the version 13.0 of odoo

--- l10n_ch_hr_payroll_ecertificate/wizard/generate_sd_wiz.py
+++ l10n_ch_hr_payroll_ecertificate/wizard/generate_sd_wiz.py
@@ -6,12 +6,12 @@ from odoo.exceptions import UserError
 
 
 class Gen_SD_Wizard(models.TransientModel):
-    _name = "hr.salary_declaration.wizard"
+    _name = "hr.salary.declaration.wizard"
     _description = "Generate salary declaration"
 
     date_from = fields.Date(string="Date from")
     date_to = fields.Date(string="Date to")
 
     def gen_sd(self):
-        sd_obj = self.env["hr.salary_declaration"]
+        sd_obj = self.env["hr.salary.declaration"]
         sd_obj.generate_yearly_declaration(self.date_from, self.date_to)
