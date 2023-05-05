27d1ad701292756372da37a0d4c77be4d2ba2a22
Simon <simon.gonzalez@bluewin.ch>
2023-05-05 11:52:23 +0200

    [IMP] l10n_ch_pain_base: black, isort, prettier

--- l10n_ch_pain_base/models/account_move_line.py
+++ l10n_ch_pain_base/models/account_move_line.py
@@ -1,11 +1,11 @@
 # copyright 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, api
+from odoo import api, models
 
 
 class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
     @api.multi
     def _prepare_payment_line_vals(self, payment_order):
@@ -16,10 +16,10 @@ class AccountMoveLine(models.Model):
             and self.invoice_id.partner_bank_id
         ):
             if self.invoice_id.partner_bank_id._is_qr_iban():
-                vals['communication_type'] = 'qrr'
+                vals["communication_type"] = "qrr"
             else:
-                vals['local_instrument'] = 'CH01'
-                vals['communication_type'] = 'isr'
-            if vals['communication']:
-                vals['communication'] = vals['communication'].replace(' ', '')
+                vals["local_instrument"] = "CH01"
+                vals["communication_type"] = "isr"
+            if vals["communication"]:
+                vals["communication"] = vals["communication"].replace(" ", "")
         return vals
