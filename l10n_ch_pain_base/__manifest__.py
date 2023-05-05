952bcd5c2d068492d6be47cd7b7e511fef92e4c9
Simon <simon.gonzalez@bluewin.ch>
2023-05-05 11:52:23 +0200

    [IMP] l10n_ch_pain_base: black, isort, prettier

--- l10n_ch_pain_base/__manifest__.py
+++ l10n_ch_pain_base/__manifest__.py
@@ -5,17 +5,12 @@
 {
     "name": "Switzerland - ISO 20022",
     "summary": "ISO 20022 base module for Switzerland",
-    "version": "12.0.2.1.1",
+    "version": "13.0.1.0.0",
     "category": "Finance",
+    "website": "https://github.com/OCA/l10n-switzerland",
     "author": "Akretion,Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "depends": [
-        "account_banking_pain_base",
-        "l10n_ch_base_bank",
-    ],
-    "data": [
-        'views/account_payment_line.xml',
-        'views/bank_payment_line.xml',
-    ],
-    'installable': True,
+    "depends": ["account_banking_pain_base", "l10n_ch_base_bank"],
+    "data": ["views/account_payment_line.xml", "views/bank_payment_line.xml"],
+    "installable": True,
 }
