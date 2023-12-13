9f85d29d0db80664b3317d798c40d68f3d212b85
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2023-12-13 16:55:10 +0100

    ebill_postfinance_stock: migrate to v16

--- ebill_postfinance_stock/__manifest__.py
+++ ebill_postfinance_stock/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "eBill Postfinance Stock",
     "summary": """Add stock integration to Postfinance eBill""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "maintainers": ["TDu"],
