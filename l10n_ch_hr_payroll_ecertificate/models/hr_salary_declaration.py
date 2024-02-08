5e2e863fb13b2548474e00e48fe3bd2fea1d1ded
davidwul <david@compassion.ch>
2024-02-08 08:47:23 +0100

    [ADD] Configuration options for payroll ecertificate (#69)
    * add configuration options
    * fix typo from too hasty conflict resolving

--- l10n_ch_hr_payroll_ecertificate/models/hr_salary_declaration.py
+++ l10n_ch_hr_payroll_ecertificate/models/hr_salary_declaration.py
@@ -17,13 +17,17 @@ class HrSalaryDeclaration(models.Model):
     employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee")
     date_from = fields.Date()
     date_to = fields.Date()
-    grossincome = fields.Integer()
+    gross_income = fields.Integer()
+    income = fields.Integer()
+    company_car = fields.Integer()
     social_ded = fields.Integer()
     bvg_lpp_ded = fields.Integer()
     year = fields.Char()
 
     @api.model
-    def generate_yearly_declaration(self, date_from, date_to):
+    def generate_yearly_declaration(
+        self, date_from, date_to, company_car, gross_income, deduction, lpp_bvg
+    ):
         payslip_lines = self.env["hr.payslip.line"].search(
             [
                 ("slip_id.date_from", ">=", date_from),
@@ -33,17 +37,20 @@ class HrSalaryDeclaration(models.Model):
         )
         employee_ids = payslip_lines.mapped("employee_id.id")
         grossincome = defaultdict(float)
+        companycar = defaultdict(float)
         social_ded = defaultdict(float)
         bvg_lpp_ded = defaultdict(float)
         d_from = defaultdict(str)
         d_to = defaultdict(str)
         for line in payslip_lines:
-            if line.code == "5000":
+            if line.salary_rule_id == gross_income:
                 grossincome[line.employee_id.id] += line.total
-            if line.name == "OBP Employee":
+            if line.salary_rule_id == lpp_bvg:
                 bvg_lpp_ded[line.employee_id.id] -= line.total
-            if line.code == "TOTAL_DED":
+            if line.salary_rule_id == deduction:
                 social_ded[line.employee_id.id] -= line.total
+            if line.salary_rule_id == company_car:
+                companycar[line.employee_id.id] -= line.total
             if (
                 line.slip_id.date_from.strftime("%Y-%m-%d")
                 < d_from[line.employee_id.id]
@@ -63,7 +70,9 @@ class HrSalaryDeclaration(models.Model):
                 "employee_id": emp,
                 "date_from": d_from[emp],
                 "date_to": d_to[emp],
-                "grossincome": math.floor(grossincome[emp]),
+                "gross_income": math.floor(grossincome[emp]),
+                "income": math.floor(grossincome[emp] - companycar[emp]),
+                "company_car": math.floor(companycar[emp]),
                 "social_ded": math.ceil(social_ded[emp] - bvg_lpp_ded[emp]),
                 "bvg_lpp_ded": math.ceil(bvg_lpp_ded[emp]),
                 "year": date_to.strftime("%Y-%m-%d")[:4],
--- l10n_ch_hr_payroll_ecertificate/wizard/hr_salary_declaration_wizard.py
+++ l10n_ch_hr_payroll_ecertificate/wizard/hr_salary_declaration_wizard.py
@@ -10,7 +10,20 @@ class GenSdWizard(models.TransientModel):
 
     date_from = fields.Date(string="Date from")
     date_to = fields.Date(string="Date to")
+    gross_income = fields.Many2one("hr.salary.rule", string="Gross income / N°8")
+    lpp_bvg = fields.Many2one("hr.salary.rule", string="BVG/LPP / N°10.1")
+    company_car = fields.Many2one(
+        "hr.salary.rule", string="Private use of company car / N°2.2"
+    )
+    deduction = fields.Many2one("hr.salary.rule", string="Total deduction / N°9")
 
     def gen_sd(self):
         sd_obj = self.env["hr.salary.declaration"]
-        sd_obj.generate_yearly_declaration(self.date_from, self.date_to)
+        sd_obj.generate_yearly_declaration(
+            self.date_from,
+            self.date_to,
+            self.company_car,
+            self.gross_income,
+            self.deduction,
+            self.lpp_bvg,
+        )
