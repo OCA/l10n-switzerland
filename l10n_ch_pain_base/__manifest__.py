e5b9f93bed3b7950187f15baa421de99a8910456
David Wulliamoz <dwulliamoz@compassion.ch>
2024-04-28 21:22:33 +0200

    [MIG] l10n_ch_pain_base: Migration to 16.0

--- l10n_ch_pain_base/__manifest__.py
+++ l10n_ch_pain_base/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Switzerland - ISO 20022",
     "summary": "ISO 20022 base module for Switzerland",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Finance",
     "website": "https://github.com/OCA/l10n-switzerland",
     "author": "Akretion,Camptocamp,Odoo Community Association (OCA)",
