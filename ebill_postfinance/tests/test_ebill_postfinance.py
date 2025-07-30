06f5e458cfb781c6fa1683c20dfad607d26ac102
Simone Orsi <simahawk@gmail.com>
2025-07-30 21:30:43 +0200

    ebill_postfinance: reduce test logs noise

--- ebill_postfinance/tests/test_ebill_postfinance.py
+++ ebill_postfinance/tests/test_ebill_postfinance.py
@@ -3,7 +3,11 @@
 
 import os
 
+from odoo.tools import mute_logger
+
 from .common import CommonCase, recorder
+
+LOGGER = "odoo.addons.ebill_postfinance.models.ebill_postfinance_service"
 
 
 class TestEbillPostfinance(CommonCase):
@@ -13,11 +17,13 @@ class TestEbillPostfinance(CommonCase):
         cls.transaction_id = "test-transaction-123"
         cls.invoice_message = cls.invoice.create_postfinance_ebill()
 
+    @mute_logger("vcr.cassette", LOGGER)
     @recorder.use_cassette
     def test_ping_service(self):
         """Check the ping service testing purpose only."""
         self.service.ping_service()
 
+    @mute_logger("vcr.cassette", LOGGER)
     @recorder.use_cassette
     def test_upload_file(self):
         """Check uploading an XML invoice to the service."""
@@ -34,6 +40,7 @@ class TestEbillPostfinance(CommonCase):
         self.assertEqual(result.TransactionID, "test-transaction-123")
         self.assertEqual(result.ProcessingState, "OK")
 
+    @mute_logger("vcr.cassette", LOGGER)
     @recorder.use_cassette
     def test_search_invoices(self):
         """Check the search invoice endpoint.
