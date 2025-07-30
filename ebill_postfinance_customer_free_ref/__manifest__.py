a21aba267a79f143f9946003d33b571b6523d1fe
Simone Orsi <simahawk@gmail.com>
2025-07-30 21:50:43 +0200

    ebill_postfinance_customer_free_ref: migrate to v18

--- ebill_postfinance_customer_free_ref/__manifest__.py
+++ ebill_postfinance_customer_free_ref/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "eBill Postfinance Customer Free Reference",
     "summary": "Glue module: ebill_postfinance and sale_order_customer_free_ref",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-switzerland",
--- ebill_postfinance_customer_free_ref/readme/CONTRIBUTORS.md
+++ ebill_postfinance_customer_free_ref/readme/CONTRIBUTORS.md
@@ -1,1 +1,2 @@
 - Thierry Ducrest \<<thierry.ducrest@camptocamp.com>\>
+- Simone Orsi \<<simone.orsi@camptocamp.com>\>
