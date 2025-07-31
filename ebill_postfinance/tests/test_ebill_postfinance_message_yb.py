edcfe2d75253fdc0cb407df844871e0f82fc5b2e
Simone Orsi <simahawk@gmail.com>
2025-07-31 08:04:47 +0200

    ebill_postfinance: avoid formatting of sample files

--- ebill_postfinance/tests/test_ebill_postfinance_message_yb.py
+++ ebill_postfinance/tests/test_ebill_postfinance_message_yb.py
@@ -50,7 +50,7 @@ class TestEbillPostfinanceMessageYB(CommonCase):
         payload = "\n".join(lines).encode("utf8")
         # Prepare the XML file that is expected
         expected_tmpl = Template(
-            file_open("ebill_postfinance/tests/examples/invoice_qr_yb.xml").read()
+            file_open("ebill_postfinance/tests/samples/invoice_qr_yb.xml").read()
         )
         expected = expected_tmpl.substitute(
             TRANSACTION_ID=message.transaction_id, CUSTOMER_ID=self.customer.id
