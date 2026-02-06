e23cf3c83da1bb6bc982c174dcd08b369c727edf
Julien Guenat <julien@jguenat.ch>
2026-02-06 19:36:21 +0100

    [IMP] l10n_ch_pain_base: Support PAIN.001.001.09.ch.03

--- l10n_ch_pain_base/__manifest__.py
+++ l10n_ch_pain_base/__manifest__.py
@@ -5,12 +5,11 @@
 {
     "name": "Switzerland - ISO 20022",
     "summary": "ISO 20022 base module for Switzerland",
-    "version": "14.0.1.0.0",
+    "version": "14.0.2.0.0",
     "category": "Finance",
     "website": "https://github.com/OCA/l10n-switzerland",
     "author": "Akretion,Camptocamp,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["account_banking_pain_base", "l10n_ch_base_bank"],
-    "data": ["views/account_payment_line.xml"],
     "installable": True,
 }
